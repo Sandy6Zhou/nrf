hakonfam/fw-nrfconnect-nrf-1

spm: create nsc function for reading flash
Create non-secure-callable function which checks an address against
a list of ranges and allows the write from non-secure side if the
address is within any of the allowed ranges.

Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

include/secure_services.h
@@ -52,6 +52,20 @@ void spm_request_system_reboot(void);
 int spm_request_random_number(u8_t *output, size_t len, size_t *olen);
 
 
+/** Request a read operation to be executed from Secure Firmware.
+ *
+ * @param[out] destination Pointer to destination array where the content is
+ *                         to be copied.
+ * @param[in]  addr        Address to be copied from.
+ * @param[in]  num         Number of bytes to copy.
+ *
+ * @retval 0        If successful.
+ * @retval -EINVAL  If destination is NULL, or if num is <= 0.
+ * @retval -EPERM   If source is outside of allowed ranges.
+ */
+int spm_request_read(void *destination, u32_t addr, size_t num);
+
+
 #ifdef __cplusplus
 }
 #endif

subsys/spm/secure_services.c
@@ -6,8 +6,14 @@
 #include <errno.h>
 #include <cortex_m/tz.h>
 #include <misc/reboot.h>
+#include <misc/util.h>
 #include <autoconf.h>
 #include <secure_services.h>
+#include <string.h>
+
+#if USE_PARTITION_MANAGER
+#include <pm_config.h>
+#endif
 
 /*
  * Secure Entry functions to allow access to secure services from non-secure
@@ -30,7 +36,7 @@
 /** @brief Structure holding the memory required to generate entropy
  */
 static mbedtls_rng_workbuf_internal rng_workbuf;
-#endif
+#endif /* CONFIG_SPM_SERVICE_RNG */
 
 
 int spm_secure_services_init(void)
@@ -46,6 +52,41 @@ int spm_secure_services_init(void)
 	return err;
 }
 
+
+#ifdef CONFIG_SPM_SERVICE_READ
+struct read_range {
+	u32_t start;
+	size_t size;
+};
+
+__TZ_NONSECURE_ENTRY_FUNC
+int spm_request_read(void *destination, u32_t addr, size_t num)
+{
+	static const struct read_range ranges[] = {
+#ifdef PM_MCUBOOT_ADDRESS
+		/* Allow reads of mcuboot metadata */
+		{.start = PM_MCUBOOT_PAD_ADDRESS,
+		 .size = PM_MCUBOOT_PAD_SIZE},
+#endif
+	};
+
+	if (destination == NULL || num <= 0) {
+		return -EINVAL;
+	}
+
+	for (size_t i = 0; i < ARRAY_SIZE(ranges); i++) {
+		u32_t start = ranges[i].start;
+		u32_t size = ranges[i].size;
+
+		if (addr >= start && addr + num <= start + size) {
+			memcpy(destination, (const void *)addr, num);
+			return 0;
+		}
+	}
+
+	return -EPERM;
+}
+#endif /* CONFIG_SPM_SERVICE_READ */
 
 #ifdef CONFIG_SPM_SERVICE_REBOOT
 __TZ_NONSECURE_ENTRY_FUNC

Sandy6Zhou/nrf

include/secure_services.h
@@ -52,6 +52,20 @@ void spm_request_system_reboot(void);
 int spm_request_random_number(u8_t *output, size_t len, size_t *olen);
 
 
+/** Request a read operation to be executed from Secure Firmware.
+ *
+ * @param[out] destination Pointer to destination array where the content is
+ *                         to be copied.
+ * @param[in]  addr        Address to be copied from.
+ * @param[in]  len         Number of bytes to copy.
+ *
+ * @retval 0        If successful.
+ * @retval -EINVAL  If destination is NULL, or if len is <= 0.
+ * @retval -EPERM   If source is outside of allowed ranges.
+ */
+int spm_request_read(void *destination, u32_t addr, size_t len);
+
+
 #ifdef __cplusplus
 }
 #endif

subsys/spm/secure_services.c
@@ -6,8 +6,14 @@
 #include <errno.h>
 #include <cortex_m/tz.h>
 #include <misc/reboot.h>
+#include <misc/util.h>
 #include <autoconf.h>
 #include <secure_services.h>
+#include <string.h>
+
+#if USE_PARTITION_MANAGER
+#include <pm_config.h>
+#endif
 
 /*
  * Secure Entry functions to allow access to secure services from non-secure
@@ -30,7 +36,7 @@
 /** @brief Structure holding the memory required to generate entropy
  */
 static mbedtls_rng_workbuf_internal rng_workbuf;
-#endif
+#endif /* CONFIG_SPM_SERVICE_RNG */
 
 
 int spm_secure_services_init(void)
@@ -46,6 +52,41 @@ int spm_secure_services_init(void)
 	return err;
 }
 
+
+#ifdef CONFIG_SPM_SERVICE_READ
+struct read_range {
+	u32_t start;
+	size_t size;
+};
+
+__TZ_NONSECURE_ENTRY_FUNC
+int spm_request_read(void *destination, u32_t addr, size_t len)
+{
+	static const struct read_range ranges[] = {
+#ifdef PM_MCUBOOT_ADDRESS
+		/* Allow reads of mcuboot metadata */
+		{.start = PM_MCUBOOT_PAD_ADDRESS,
+		 .size = PM_MCUBOOT_PAD_SIZE},
+#endif
+	};
+
+	if (destination == NULL || len <= 0) {
+		return -EINVAL;
+	}
+
+	for (size_t i = 0; i < ARRAY_SIZE(ranges); i++) {
+		u32_t start = ranges[i].start;
+		u32_t size = ranges[i].size;
+
+		if (addr >= start && addr + len <= start + size) {
+			memcpy(destination, (const void *)addr, len);
+			return 0;
+		}
+	}
+
+	return -EPERM;
+}
+#endif /* CONFIG_SPM_SERVICE_READ */
 
 #ifdef CONFIG_SPM_SERVICE_REBOOT
 __TZ_NONSECURE_ENTRY_FUNC