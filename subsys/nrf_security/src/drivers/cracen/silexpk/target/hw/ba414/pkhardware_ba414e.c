nrfconnect/sdk-nrf

nrf_security: cracen: Add workaround kconfig
Make the CRACEN lite IKG workaround not depend on CRACEN_HW_VERSION_LITE
Add Kconfig for the workaround instead
This allows for CRACEN LITE variants that do not use the workaround

Signed-off-by: Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>

subsys/nrf_security/src/drivers/cracen/silexpk/target/hw/ba414/pkhardware_ba414e.c
@@ -295,7 +295,7 @@ static void run_ik_cmd(sx_pk_req *req)
 	sx_pk_wrreg(&req->regs, IK_REG_PK_CONTROL,
 		    IK_PK_CONTROL_START_OP | IK_PK_CONTROL_CLEAR_IRQ);
 
-	if (IS_ENABLED(CONFIG_CRACEN_HW_VERSION_LITE)) {
+	if (IS_ENABLED(CONFIG_CRACEN_USE_IKG_INTERRUPT_WORKAROUND)) {
 		/* Workaround to handle IKG freezing on CRACEN lite.
 		 * THE PKE-IKG interrupt can not be cleared from software, but can
 		 * be cleared by hardware when in PK mode.

Sandy6Zhou/nrf

subsys/nrf_security/src/drivers/cracen/silexpk/target/hw/ba414/pkhardware_ba414e.c
@@ -295,7 +295,7 @@ static void run_ik_cmd(sx_pk_req *req)
 	sx_pk_wrreg(&req->regs, IK_REG_PK_CONTROL,
 		    IK_PK_CONTROL_START_OP | IK_PK_CONTROL_CLEAR_IRQ);
 
-	if (IS_ENABLED(CONFIG_CRACEN_HW_VERSION_LITE)) {
+	if (IS_ENABLED(CONFIG_CRACEN_NEED_IKG_INTERRUPT_WORKAROUND)) {
 		/* Workaround to handle IKG freezing on CRACEN lite.
 		 * THE PKE-IKG interrupt can not be cleared from software, but can
 		 * be cleared by hardware when in PK mode.