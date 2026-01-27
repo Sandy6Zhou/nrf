nrfconnect/sdk-nrf

samples: matter: lock: Support all fields of LockOperation event
Add support for `UserIndex`, `FabricIndex`, `SourceNode` and
`Credentials` fields.

Signed-off-by: Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>

samples/matter/lock/src/app_task.cpp
@@ -130,9 +130,9 @@ void AppTask::SwitchTransportTriggerHandler(const SwitchButtonAction &action)
 }
 #endif
 
-void AppTask::LockStateChanged(BoltLockManager::State state, BoltLockManager::OperationSource source)
+void AppTask::LockStateChanged(const BoltLockManager::StateData &stateData)
 {
-	switch (state) {
+	switch (stateData.state) {
 	case BoltLockManager::State::kLockingInitiated:
 		LOG_INF("Lock action initiated");
 		Nrf::GetBoard().GetLED(Nrf::DeviceLeds::LED2).Blink(50, 50);
@@ -164,26 +164,33 @@ void AppTask::LockStateChanged(BoltLockManager::State state, BoltLockManager::Op
 	}
 
 	/* Handle changing attribute state in the application */
-	Instance().UpdateClusterState(state, source);
+	Instance().UpdateClusterState(stateData);
 }
 
-void AppTask::UpdateClusterState(BoltLockManager::State state, BoltLockManager::OperationSource source)
+void AppTask::UpdateClusterState(const BoltLockManager::StateData &stateData)
 {
-	DlLockState newLockState;
+	BoltLockManager::StateData *stateDataCopy = Platform::New<BoltLockManager::StateData>(stateData);
 
-	switch (state) {
-	case BoltLockManager::State::kLockingCompleted:
-		newLockState = DlLockState::kLocked;
-		break;
-	case BoltLockManager::State::kUnlockingCompleted:
-		newLockState = DlLockState::kUnlocked;
-		break;
-	default:
-		newLockState = DlLockState::kNotFullyLocked;
-		break;
+	if (stateDataCopy == nullptr) {
+		LOG_ERR("Failed to allocate memory for BoltLockManager::StateData");
+		return;
 	}
 
-	SystemLayer().ScheduleLambda([newLockState, source] {
+	SystemLayer().ScheduleLambda([stateData = stateDataCopy]() {
+		DlLockState newLockState;
+
+		switch (stateData->state) {
+		case BoltLockManager::State::kLockingCompleted:
+			newLockState = DlLockState::kLocked;
+			break;
+		case BoltLockManager::State::kUnlockingCompleted:
+			newLockState = DlLockState::kUnlocked;
+			break;
+		default:
+			newLockState = DlLockState::kNotFullyLocked;
+			break;
+		}
+
 		chip::app::DataModel::Nullable<chip::app::Clusters::DoorLock::DlLockState> currentLockState;
 		chip::app::Clusters::DoorLock::Attributes::LockState::Get(kLockEndpointId, currentLockState);
 
@@ -193,10 +200,29 @@ void AppTask::UpdateClusterState(BoltLockManager::State state, BoltLockManager::
 		} else {
 			LOG_INF("Updating LockState attribute");
 
-			if (!DoorLockServer::Instance().SetLockState(kLockEndpointId, newLockState, source)) {
+			Nullable<uint16_t> userId;
+			Nullable<List<const LockOpCredentials>> credentials;
+			List<const LockOpCredentials> credentialList;
+
+			if (!stateData->validatePINResult.IsNull()) {
+				userId = { stateData->validatePINResult.Value().userId };
+
+				/* `DoorLockServer::SetLockState` exptects list of `LockOpCredentials`,
+				   however in case of PIN validation it makes no sense to have more than one
+				   credential corresponding to validation result. For simplicity we wrap single
+				   credential in list here. */
+				credentialList = { &stateData->validatePINResult.Value().credential, 1 };
+				credentials = { credentialList };
+			}
+
+			if (!DoorLockServer::Instance().SetLockState(kLockEndpointId, newLockState, stateData->source,
+								     userId, credentials, stateData->fabricIdx,
+								     stateData->nodeId)) {
 				LOG_ERR("Failed to update LockState attribute");
 			}
 		}
+
+		Platform::Delete(stateData);
 	});
 }
 
@@ -204,8 +230,8 @@ void AppTask::UpdateClusterState(BoltLockManager::State state, BoltLockManager::
 void AppTask::NUSLockCallback(void *context)
 {
 	LOG_DBG("Received LOCK command from NUS");
-	if (BoltLockMgr().mState == BoltLockManager::State::kLockingCompleted ||
-	    BoltLockMgr().mState == BoltLockManager::State::kLockingInitiated) {
+	if (BoltLockMgr().GetState().state == BoltLockManager::State::kLockingCompleted ||
+	    BoltLockMgr().GetState().state == BoltLockManager::State::kLockingInitiated) {
 		LOG_INF("Device is already locked");
 	} else {
 		Nrf::PostTask([] { LockActionEventHandler(); });
@@ -215,8 +241,8 @@ void AppTask::NUSLockCallback(void *context)
 void AppTask::NUSUnlockCallback(void *context)
 {
 	LOG_DBG("Received UNLOCK command from NUS");
-	if (BoltLockMgr().mState == BoltLockManager::State::kUnlockingCompleted ||
-	    BoltLockMgr().mState == BoltLockManager::State::kUnlockingInitiated) {
+	if (BoltLockMgr().GetState().state == BoltLockManager::State::kUnlockingCompleted ||
+	    BoltLockMgr().GetState().state == BoltLockManager::State::kUnlockingInitiated) {
 		LOG_INF("Device is already unlocked");
 	} else {
 		Nrf::PostTask([] { LockActionEventHandler(); });

samples/matter/lock/src/bolt_lock_manager.cpp
@@ -111,21 +111,23 @@ bool BoltLockManager::GetRequirePIN()
 	return AccessMgr::Instance().GetRequirePIN();
 }
 
-void BoltLockManager::Lock(OperationSource source)
+void BoltLockManager::Lock(const OperationSource source, const Nullable<chip::FabricIndex> &fabricIdx,
+			   const Nullable<chip::NodeId> &nodeId, const Nullable<ValidatePINResult> &validatePINResult)
 {
-	VerifyOrReturn(mState != State::kLockingCompleted);
-	SetState(State::kLockingInitiated, source);
+	VerifyOrReturn(mStateData.state != State::kLockingCompleted);
+	StateData newStateData{ State::kLockingInitiated, source, fabricIdx, nodeId, validatePINResult };
+	SetStateData(newStateData);
 
-	mActuatorOperationSource = source;
 	k_timer_start(&mActuatorTimer, K_MSEC(kActuatorMovementTimeMs), K_NO_WAIT);
 }
 
-void BoltLockManager::Unlock(OperationSource source)
+void BoltLockManager::Unlock(const OperationSource source, const Nullable<chip::FabricIndex> &fabricIdx,
+			     const Nullable<chip::NodeId> &nodeId, const Nullable<ValidatePINResult> &validatePINResult)
 {
-	VerifyOrReturn(mState != State::kUnlockingCompleted);
-	SetState(State::kUnlockingInitiated, source);
+	VerifyOrReturn(mStateData.state != State::kUnlockingCompleted);
+	StateData newStateData{ State::kUnlockingInitiated, source, fabricIdx, nodeId, validatePINResult };
+	SetStateData(newStateData);
 
-	mActuatorOperationSource = source;
 	k_timer_start(&mActuatorTimer, K_MSEC(kActuatorMovementTimeMs), K_NO_WAIT);
 }
 
@@ -146,24 +148,33 @@ void BoltLockManager::ActuatorAppEventHandler(const BoltLockManagerEvent &event)
 {
 	BoltLockManager *lock = reinterpret_cast<BoltLockManager *>(event.manager);
 
-	switch (lock->mState) {
+	switch (lock->mStateData.state) {
 	case State::kLockingInitiated:
-		lock->SetState(State::kLockingCompleted, lock->mActuatorOperationSource);
+		lock->SetState(State::kLockingCompleted);
 		break;
 	case State::kUnlockingInitiated:
-		lock->SetState(State::kUnlockingCompleted, lock->mActuatorOperationSource);
+		lock->SetState(State::kUnlockingCompleted);
 		break;
 	default:
 		break;
 	}
 }
 
-void BoltLockManager::SetState(State state, OperationSource source)
+void BoltLockManager::SetState(State state)
 {
-	mState = state;
+	mStateData.state = state;
 
 	if (mStateChangeCallback != nullptr) {
-		mStateChangeCallback(state, source);
+		mStateChangeCallback(mStateData);
+	}
+}
+
+void BoltLockManager::SetStateData(const StateData &stateData)
+{
+	mStateData = stateData;
+
+	if (mStateChangeCallback != nullptr) {
+		mStateChangeCallback(mStateData);
 	}
 }
 

samples/matter/lock/src/bolt_lock_manager.h
@@ -40,15 +40,24 @@ public:
 	};
 
 	using OperationSource = chip::app::Clusters::DoorLock::OperationSourceEnum;
-	using StateChangeCallback = void (*)(State, OperationSource);
 	using ValidatePINResult = AccessMgr::ValidatePINResult;
+
+	struct StateData {
+		State state;
+		OperationSource source;
+		Nullable<chip::FabricIndex> fabricIdx;
+		Nullable<chip::NodeId> nodeId;
+		Nullable<ValidatePINResult> validatePINResult;
+	};
+
+	using StateChangeCallback = void (*)(const StateData &);
 
 	static constexpr uint32_t kActuatorMovementTimeMs = 2000;
 
 	void Init(StateChangeCallback callback);
 
-	State GetState() const { return mState; }
-	bool IsLocked() const { return mState == State::kLockingCompleted; }
+	const StateData &GetState() const { return mStateData; }
+	bool IsLocked() const { return mStateData.state == State::kLockingCompleted; }
 
 	bool GetUser(uint16_t userIndex, EmberAfPluginDoorLockUserInfo &user);
 	bool SetUser(uint16_t userIndex, chip::FabricIndex creator, chip::FabricIndex modifier,
@@ -80,8 +89,12 @@ public:
 	bool ValidatePIN(const Optional<chip::ByteSpan> &pinCode, OperationErrorEnum &err,
 			 Nullable<ValidatePINResult> &result);
 
-	void Lock(OperationSource source);
-	void Unlock(OperationSource source);
+	void Lock(const OperationSource source, const Nullable<chip::FabricIndex> &fabricIdx = NullNullable,
+		  const Nullable<chip::NodeId> &nodeId = NullNullable,
+		  const Nullable<ValidatePINResult> &validatePINResult = NullNullable);
+	void Unlock(const OperationSource source, const Nullable<chip::FabricIndex> &fabricIdx = NullNullable,
+		    const Nullable<chip::NodeId> &nodeId = NullNullable,
+		    const Nullable<ValidatePINResult> &validatePINResult = NullNullable);
 
 	void SetRequirePIN(bool require);
 	bool GetRequirePIN();
@@ -91,15 +104,15 @@ public:
 private:
 	friend class AppTask;
 
-	void SetState(State state, OperationSource source);
+	void SetState(State state);
+	void SetStateData(const StateData &stateData);
 
 	static void ActuatorTimerEventHandler(k_timer *timer);
 	static void ActuatorAppEventHandler(const BoltLockManagerEvent &event);
 	friend BoltLockManager &BoltLockMgr();
 
-	State mState = State::kLockingCompleted;
+	StateData mStateData = { State::kLockingCompleted, OperationSource::kButton, {}, {}, {} };
 	StateChangeCallback mStateChangeCallback = nullptr;
-	OperationSource mActuatorOperationSource = OperationSource::kButton;
 	k_timer mActuatorTimer = {};
 
 	static BoltLockManager sLock;

samples/matter/lock/src/zcl_callbacks.cpp
@@ -68,7 +68,7 @@ bool emberAfPluginDoorLockOnDoorLockCommand(EndpointId endpointId, const Nullabl
 
 	/* Handle changing attribute state on command reception */
 	if (success) {
-		BoltLockMgr().Lock(BoltLockManager::OperationSource::kRemote);
+		BoltLockMgr().Lock(BoltLockManager::OperationSource::kRemote, fabricIdx, nodeId, validatePINResult);
 	}
 
 	return success;
@@ -83,7 +83,7 @@ bool emberAfPluginDoorLockOnDoorUnlockCommand(EndpointId endpointId, const Nulla
 
 	/* Handle changing attribute state on command reception */
 	if (success) {
-		BoltLockMgr().Unlock(BoltLockManager::OperationSource::kRemote);
+		BoltLockMgr().Unlock(BoltLockManager::OperationSource::kRemote, fabricIdx, nodeId, validatePINResult);
 	}
 
 	return success;
@@ -124,8 +124,9 @@ void emberAfDoorLockClusterInitCallback(EndpointId endpoint)
 		     "number of holiday schedules");
 #endif /* CONFIG_LOCK_SCHEDULES */
 
-	AppTask::Instance().UpdateClusterState(BoltLockMgr().GetState(),
-					       BoltLockManager::OperationSource::kUnspecified);
+	BoltLockManager::StateData state = BoltLockMgr().GetState();
+	state.source = BoltLockManager::OperationSource::kUnspecified;
+	AppTask::Instance().UpdateClusterState(state);
 }
 
 #ifdef CONFIG_LOCK_SCHEDULES

Sandy6Zhou/nrf

samples/matter/lock/src/app_task.cpp
@@ -130,9 +130,9 @@ void AppTask::SwitchTransportTriggerHandler(const SwitchButtonAction &action)
 }
 #endif
 
-void AppTask::LockStateChanged(BoltLockManager::State state, BoltLockManager::OperationSource source)
+void AppTask::LockStateChanged(const BoltLockManager::StateData &stateData)
 {
-	switch (state) {
+	switch (stateData.mState) {
 	case BoltLockManager::State::kLockingInitiated:
 		LOG_INF("Lock action initiated");
 		Nrf::GetBoard().GetLED(Nrf::DeviceLeds::LED2).Blink(50, 50);
@@ -164,26 +164,33 @@ void AppTask::LockStateChanged(BoltLockManager::State state, BoltLockManager::Op
 	}
 
 	/* Handle changing attribute state in the application */
-	Instance().UpdateClusterState(state, source);
+	Instance().UpdateClusterState(stateData);
 }
 
-void AppTask::UpdateClusterState(BoltLockManager::State state, BoltLockManager::OperationSource source)
+void AppTask::UpdateClusterState(const BoltLockManager::StateData &stateData)
 {
-	DlLockState newLockState;
+	BoltLockManager::StateData *stateDataCopy = Platform::New<BoltLockManager::StateData>(stateData);
 
-	switch (state) {
-	case BoltLockManager::State::kLockingCompleted:
-		newLockState = DlLockState::kLocked;
-		break;
-	case BoltLockManager::State::kUnlockingCompleted:
-		newLockState = DlLockState::kUnlocked;
-		break;
-	default:
-		newLockState = DlLockState::kNotFullyLocked;
-		break;
+	if (stateDataCopy == nullptr) {
+		LOG_ERR("Failed to allocate memory for BoltLockManager::StateData");
+		return;
 	}
 
-	SystemLayer().ScheduleLambda([newLockState, source] {
+	CHIP_ERROR err = SystemLayer().ScheduleLambda([stateData = stateDataCopy]() {
+		DlLockState newLockState;
+
+		switch (stateData->mState) {
+		case BoltLockManager::State::kLockingCompleted:
+			newLockState = DlLockState::kLocked;
+			break;
+		case BoltLockManager::State::kUnlockingCompleted:
+			newLockState = DlLockState::kUnlocked;
+			break;
+		default:
+			newLockState = DlLockState::kNotFullyLocked;
+			break;
+		}
+
 		chip::app::DataModel::Nullable<chip::app::Clusters::DoorLock::DlLockState> currentLockState;
 		chip::app::Clusters::DoorLock::Attributes::LockState::Get(kLockEndpointId, currentLockState);
 
@@ -193,19 +200,43 @@ void AppTask::UpdateClusterState(BoltLockManager::State state, BoltLockManager::
 		} else {
 			LOG_INF("Updating LockState attribute");
 
-			if (!DoorLockServer::Instance().SetLockState(kLockEndpointId, newLockState, source)) {
+			Nullable<uint16_t> userId;
+			Nullable<List<const LockOpCredentials>> credentials;
+			List<const LockOpCredentials> credentialList;
+
+			if (!stateData->mValidatePINResult.IsNull()) {
+				userId = { stateData->mValidatePINResult.Value().mUserId };
+
+				/* `DoorLockServer::SetLockState` exptects list of `LockOpCredentials`,
+				   however in case of PIN validation it makes no sense to have more than one
+				   credential corresponding to validation result. For simplicity we wrap single
+				   credential in list here. */
+				credentialList = { &stateData->mValidatePINResult.Value().mCredential, 1 };
+				credentials = { credentialList };
+			}
+
+			if (!DoorLockServer::Instance().SetLockState(kLockEndpointId, newLockState, stateData->mSource,
+								     userId, credentials, stateData->mFabricIdx,
+								     stateData->mNodeId)) {
 				LOG_ERR("Failed to update LockState attribute");
 			}
 		}
+
+		Platform::Delete(stateData);
 	});
+
+	if (err != CHIP_NO_ERROR) {
+		LOG_ERR("Failed to schedule lambda: %" CHIP_ERROR_FORMAT, err.Format());
+		Platform::Delete(stateDataCopy);
+	}
 }
 
 #ifdef CONFIG_CHIP_NUS
 void AppTask::NUSLockCallback(void *context)
 {
 	LOG_DBG("Received LOCK command from NUS");
-	if (BoltLockMgr().mState == BoltLockManager::State::kLockingCompleted ||
-	    BoltLockMgr().mState == BoltLockManager::State::kLockingInitiated) {
+	if (BoltLockMgr().GetState().mState == BoltLockManager::State::kLockingCompleted ||
+	    BoltLockMgr().GetState().mState == BoltLockManager::State::kLockingInitiated) {
 		LOG_INF("Device is already locked");
 	} else {
 		Nrf::PostTask([] { LockActionEventHandler(); });
@@ -215,8 +246,8 @@ void AppTask::NUSLockCallback(void *context)
 void AppTask::NUSUnlockCallback(void *context)
 {
 	LOG_DBG("Received UNLOCK command from NUS");
-	if (BoltLockMgr().mState == BoltLockManager::State::kUnlockingCompleted ||
-	    BoltLockMgr().mState == BoltLockManager::State::kUnlockingInitiated) {
+	if (BoltLockMgr().GetState().mState == BoltLockManager::State::kUnlockingCompleted ||
+	    BoltLockMgr().GetState().mState == BoltLockManager::State::kUnlockingInitiated) {
 		LOG_INF("Device is already unlocked");
 	} else {
 		Nrf::PostTask([] { LockActionEventHandler(); });

samples/matter/lock/src/bolt_lock_manager.cpp
@@ -111,21 +111,23 @@ bool BoltLockManager::GetRequirePIN()
 	return AccessMgr::Instance().GetRequirePIN();
 }
 
-void BoltLockManager::Lock(OperationSource source)
+void BoltLockManager::Lock(const OperationSource source, const Nullable<chip::FabricIndex> &fabricIdx,
+			   const Nullable<chip::NodeId> &nodeId, const Nullable<ValidatePINResult> &validatePINResult)
 {
-	VerifyOrReturn(mState != State::kLockingCompleted);
-	SetState(State::kLockingInitiated, source);
+	VerifyOrReturn(mStateData.mState != State::kLockingCompleted);
+	StateData newStateData{ State::kLockingInitiated, source, fabricIdx, nodeId, validatePINResult };
+	SetStateData(newStateData);
 
-	mActuatorOperationSource = source;
 	k_timer_start(&mActuatorTimer, K_MSEC(kActuatorMovementTimeMs), K_NO_WAIT);
 }
 
-void BoltLockManager::Unlock(OperationSource source)
+void BoltLockManager::Unlock(const OperationSource source, const Nullable<chip::FabricIndex> &fabricIdx,
+			     const Nullable<chip::NodeId> &nodeId, const Nullable<ValidatePINResult> &validatePINResult)
 {
-	VerifyOrReturn(mState != State::kUnlockingCompleted);
-	SetState(State::kUnlockingInitiated, source);
+	VerifyOrReturn(mStateData.mState != State::kUnlockingCompleted);
+	StateData newStateData{ State::kUnlockingInitiated, source, fabricIdx, nodeId, validatePINResult };
+	SetStateData(newStateData);
 
-	mActuatorOperationSource = source;
 	k_timer_start(&mActuatorTimer, K_MSEC(kActuatorMovementTimeMs), K_NO_WAIT);
 }
 
@@ -146,24 +148,33 @@ void BoltLockManager::ActuatorAppEventHandler(const BoltLockManagerEvent &event)
 {
 	BoltLockManager *lock = reinterpret_cast<BoltLockManager *>(event.manager);
 
-	switch (lock->mState) {
+	switch (lock->mStateData.mState) {
 	case State::kLockingInitiated:
-		lock->SetState(State::kLockingCompleted, lock->mActuatorOperationSource);
+		lock->SetState(State::kLockingCompleted);
 		break;
 	case State::kUnlockingInitiated:
-		lock->SetState(State::kUnlockingCompleted, lock->mActuatorOperationSource);
+		lock->SetState(State::kUnlockingCompleted);
 		break;
 	default:
 		break;
 	}
 }
 
-void BoltLockManager::SetState(State state, OperationSource source)
+void BoltLockManager::SetState(State state)
 {
-	mState = state;
+	mStateData.mState = state;
 
 	if (mStateChangeCallback != nullptr) {
-		mStateChangeCallback(state, source);
+		mStateChangeCallback(mStateData);
+	}
+}
+
+void BoltLockManager::SetStateData(const StateData &stateData)
+{
+	mStateData = stateData;
+
+	if (mStateChangeCallback != nullptr) {
+		mStateChangeCallback(mStateData);
 	}
 }
 

samples/matter/lock/src/bolt_lock_manager.h
@@ -40,15 +40,24 @@ public:
 	};
 
 	using OperationSource = chip::app::Clusters::DoorLock::OperationSourceEnum;
-	using StateChangeCallback = void (*)(State, OperationSource);
 	using ValidatePINResult = AccessMgr::ValidatePINResult;
+
+	struct StateData {
+		State mState;
+		OperationSource mSource;
+		Nullable<chip::FabricIndex> mFabricIdx;
+		Nullable<chip::NodeId> mNodeId;
+		Nullable<ValidatePINResult> mValidatePINResult;
+	};
+
+	using StateChangeCallback = void (*)(const StateData &);
 
 	static constexpr uint32_t kActuatorMovementTimeMs = 2000;
 
 	void Init(StateChangeCallback callback);
 
-	State GetState() const { return mState; }
-	bool IsLocked() const { return mState == State::kLockingCompleted; }
+	const StateData &GetState() const { return mStateData; }
+	bool IsLocked() const { return mStateData.mState == State::kLockingCompleted; }
 
 	bool GetUser(uint16_t userIndex, EmberAfPluginDoorLockUserInfo &user);
 	bool SetUser(uint16_t userIndex, chip::FabricIndex creator, chip::FabricIndex modifier,
@@ -80,8 +89,12 @@ public:
 	bool ValidatePIN(const Optional<chip::ByteSpan> &pinCode, OperationErrorEnum &err,
 			 Nullable<ValidatePINResult> &result);
 
-	void Lock(OperationSource source);
-	void Unlock(OperationSource source);
+	void Lock(const OperationSource source, const Nullable<chip::FabricIndex> &fabricIdx = NullNullable,
+		  const Nullable<chip::NodeId> &nodeId = NullNullable,
+		  const Nullable<ValidatePINResult> &validatePINResult = NullNullable);
+	void Unlock(const OperationSource source, const Nullable<chip::FabricIndex> &fabricIdx = NullNullable,
+		    const Nullable<chip::NodeId> &nodeId = NullNullable,
+		    const Nullable<ValidatePINResult> &validatePINResult = NullNullable);
 
 	void SetRequirePIN(bool require);
 	bool GetRequirePIN();
@@ -91,15 +104,15 @@ public:
 private:
 	friend class AppTask;
 
-	void SetState(State state, OperationSource source);
+	void SetState(State state);
+	void SetStateData(const StateData &stateData);
 
 	static void ActuatorTimerEventHandler(k_timer *timer);
 	static void ActuatorAppEventHandler(const BoltLockManagerEvent &event);
 	friend BoltLockManager &BoltLockMgr();
 
-	State mState = State::kLockingCompleted;
+	StateData mStateData = { State::kLockingCompleted, OperationSource::kButton, {}, {}, {} };
 	StateChangeCallback mStateChangeCallback = nullptr;
-	OperationSource mActuatorOperationSource = OperationSource::kButton;
 	k_timer mActuatorTimer = {};
 
 	static BoltLockManager sLock;

samples/matter/lock/src/zcl_callbacks.cpp
@@ -68,7 +68,7 @@ bool emberAfPluginDoorLockOnDoorLockCommand(EndpointId endpointId, const Nullabl
 
 	/* Handle changing attribute state on command reception */
 	if (success) {
-		BoltLockMgr().Lock(BoltLockManager::OperationSource::kRemote);
+		BoltLockMgr().Lock(BoltLockManager::OperationSource::kRemote, fabricIdx, nodeId, validatePINResult);
 	}
 
 	return success;
@@ -83,7 +83,7 @@ bool emberAfPluginDoorLockOnDoorUnlockCommand(EndpointId endpointId, const Nulla
 
 	/* Handle changing attribute state on command reception */
 	if (success) {
-		BoltLockMgr().Unlock(BoltLockManager::OperationSource::kRemote);
+		BoltLockMgr().Unlock(BoltLockManager::OperationSource::kRemote, fabricIdx, nodeId, validatePINResult);
 	}
 
 	return success;
@@ -124,8 +124,9 @@ void emberAfDoorLockClusterInitCallback(EndpointId endpoint)
 		     "number of holiday schedules");
 #endif /* CONFIG_LOCK_SCHEDULES */
 
-	AppTask::Instance().UpdateClusterState(BoltLockMgr().GetState(),
-					       BoltLockManager::OperationSource::kUnspecified);
+	BoltLockManager::StateData state = BoltLockMgr().GetState();
+	state.mSource = BoltLockManager::OperationSource::kUnspecified;
+	AppTask::Instance().UpdateClusterState(state);
 }
 
 #ifdef CONFIG_LOCK_SCHEDULES