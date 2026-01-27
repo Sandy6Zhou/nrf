ludvigsj/sdk-nrf

Bluetooth: Mesh: Rewrite sensor API to use raw values
This commit introduces a new API for the Mesh sensor models, where the
sensor data is kept as the raw encoded values. The old APIs based on
struct sensor_value are deprecated and toggleable using a KConfig
option.

This change is made in order to be able to accurately handle all values
of every sensor format.

Signed-off-by: Ludvig Jordet <ludvig.jordet@nordicsemi.no>

include/bluetooth/mesh/sensor_srv.h
@@ -94,6 +94,30 @@ struct bt_mesh_sensor_srv {
 	struct bt_mesh_model *model;
 };
 
+#if !defined(CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE) || defined(__DOXYGEN__)
+/** @brief Publish a sensor value.
+ *
+ *  Immediately publishes the given sensor value, without checking thresholds
+ *  or intervals.
+ *
+ *  @see bt_mesh_sensor_srv_pub
+ *
+ *  @param[in] srv    Sensor server instance.
+ *  @param[in] ctx    Message context to publish with, or NULL to publish on the
+ *                    configured publish parameters.
+ *  @param[in] sensor Sensor to publish with.
+ *  @param[in] value  Sensor value to publish, interpreted as an array of sensor
+ *                    channel values matching the sensor channels specified by
+ *                    the sensor type. The length of the array must match the
+ *                    sensor channel count.
+ *
+ *  @return 0 on success, or (negative) error code otherwise.
+ */
+int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
+			   struct bt_mesh_msg_ctx *ctx,
+			   struct bt_mesh_sensor *sensor,
+			   const struct bt_mesh_sensor_value *value);
+#else /* defined(CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE) */
 /** @brief Publish a sensor value.
  *
  *  Immediately publishes the given sensor value, without checking thresholds
@@ -116,7 +140,7 @@ int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
 			   struct bt_mesh_msg_ctx *ctx,
 			   struct bt_mesh_sensor *sensor,
 			   const struct sensor_value *value);
-
+#endif /* !defined(CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE) */
 
 /** @brief Make the server to take a sample of the sensor, and publish if the
  *         value changed sufficiently.

subsys/bluetooth/mesh/sensor.h
@@ -130,9 +130,55 @@ extern const struct bt_mesh_sensor_unit bt_mesh_sensor_unit_unitless;
 
 /** @} */
 
+/* Temporary typedef while we need to support two different sensor value types
+ * in the deprecation period. Can be removed once support for
+ * struct sensor_value is removed, and internal APIs changed to use
+ * struct bt_mesh_sensor_value.
+ */
+#ifdef CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE
+typedef struct sensor_value sensor_value_type;
+#else
+typedef struct bt_mesh_sensor_value sensor_value_type;
+#endif
+
+#ifdef CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE
+#define SENSOR_VALUE_IN_RANGE(_value, _start, _end) (                          \
+		((_value)->val1 > (_start)->val1 ||                            \
+		 ((_value)->val1 == (_start)->val1 &&                          \
+		  (_value)->val2 >= (_start)->val2)) &&                        \
+		((_value)->val1 < (_end)->val1 ||                              \
+		 ((_value)->val1 == (_end)->val1 &&                            \
+		  (_value)->val2 <= (_end)->val2)))
+#else
+#define SENSOR_VALUE_IN_RANGE(_value, _start, _end) (                          \
+		(_value)->format->compare((_value), (_start)) >= 0 &&          \
+		(_value)->format->compare((_end), (_value)) >= 0)
+
+/** @brief Check if a value change breaks the delta threshold.
+ *
+ *  Sensors should publish their value if the measured sample is outside the
+ *  delta threshold compared to the previously published value. This function
+ *  checks the threshold and the previously published value for this sensor,
+ *  and returns whether the sensor should publish its value.
+ *
+ *  @note Only single-channel sensors support cadence. Multi-channel sensors are
+ *        always considered out of their threshold range, and will always return
+ *        true from this function. Single-channel sensors that haven't been
+ *        assigned a threshold will return true if the value is different.
+ *
+ *  @param[in] sensor    The sensor instance.
+ *  @param[in] value     Sensor value.
+ *
+ *  @return true if the difference between the measurements exceeds the delta
+ *          threshold, false otherwise.
+ */
+bool bt_mesh_sensor_delta_threshold(const struct bt_mesh_sensor *sensor,
+				    const struct bt_mesh_sensor_value *value);
+#endif /* CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE */
+
 int sensor_status_encode(struct net_buf_simple *buf,
 			 const struct bt_mesh_sensor *sensor,
-			 const struct sensor_value *values);
+			 const sensor_value_type *values);
 
 int sensor_status_id_encode(struct net_buf_simple *buf, uint8_t len, uint16_t id);
 void sensor_status_id_decode(struct net_buf_simple *buf, uint8_t *len, uint16_t *id);
@@ -144,17 +190,17 @@ void sensor_descriptor_encode(struct net_buf_simple *buf,
 
 int sensor_value_encode(struct net_buf_simple *buf,
 			const struct bt_mesh_sensor_type *type,
-			const struct sensor_value *values);
+			const sensor_value_type *values);
 int sensor_value_decode(struct net_buf_simple *buf,
 			const struct bt_mesh_sensor_type *type,
-			struct sensor_value *values);
+			sensor_value_type *values);
 
 int sensor_ch_encode(struct net_buf_simple *buf,
 		     const struct bt_mesh_sensor_format *format,
-		     const struct sensor_value *value);
+		     const sensor_value_type *value);
 int sensor_ch_decode(struct net_buf_simple *buf,
 		     const struct bt_mesh_sensor_format *format,
-		     struct sensor_value *value);
+		     sensor_value_type *value);
 
 int sensor_column_value_encode(struct net_buf_simple *buf,
 			       struct bt_mesh_sensor_srv *srv,
@@ -169,7 +215,7 @@ int sensor_column_encode(struct net_buf_simple *buf,
 int sensor_column_decode(
 	struct net_buf_simple *buf, const struct bt_mesh_sensor_type *type,
 	struct bt_mesh_sensor_column *col,
-	struct sensor_value value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX]);
+	sensor_value_type value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX]);
 
 int sensor_cadence_encode(struct net_buf_simple *buf,
 			  const struct bt_mesh_sensor_type *sensor_type,
@@ -188,7 +234,7 @@ uint64_t sensor_powtime_decode_us(uint8_t val);
 uint8_t sensor_pub_div_get(const struct bt_mesh_sensor *s, uint32_t base_period);
 
 void sensor_cadence_update(struct bt_mesh_sensor *sensor,
-			   const struct sensor_value *value);
+			   const sensor_value_type *value);
 
 #ifdef __cplusplus
 }

subsys/bluetooth/mesh/sensor_srv.c
@@ -80,7 +80,7 @@ static void cadence_store(struct bt_mesh_sensor_srv *srv)
 static int value_get(struct bt_mesh_sensor_srv *srv,
 		     struct bt_mesh_sensor *sensor,
 		     struct bt_mesh_msg_ctx *ctx,
-		     struct sensor_value *value)
+		     sensor_value_type *value)
 {
 	int err;
 
@@ -104,7 +104,7 @@ static int buf_status_add(struct bt_mesh_sensor_srv *srv,
 			  struct bt_mesh_msg_ctx *ctx,
 			  struct net_buf_simple *buf)
 {
-	struct sensor_value value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
+	sensor_value_type value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
 	struct net_buf_simple_state state;
 	int err;
 
@@ -223,11 +223,15 @@ respond:
 
 static const struct bt_mesh_sensor_column *
 column_get(const struct bt_mesh_sensor_series *series,
-	   const struct sensor_value *val)
+	   const sensor_value_type *val)
 {
 	for (uint32_t i = 0; i < series->column_count; ++i) {
+#ifdef CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE
 		if (series->columns[i].start.val1 == val->val1 &&
 		    series->columns[i].start.val2 == val->val2) {
+#else
+		if (val->format->compare(&series->columns[i].start, val) == 0) {
+#endif
 			return &series->columns[i];
 		}
 	}
@@ -287,7 +291,7 @@ static int handle_column_get(struct bt_mesh_model *model, struct bt_mesh_msg_ctx
 
 	const struct bt_mesh_sensor_format *col_format;
 	const struct bt_mesh_sensor_column *col;
-	struct sensor_value col_x;
+	sensor_value_type col_x;
 
 	col_format = bt_mesh_sensor_column_format_get(sensor->type);
 
@@ -403,18 +407,18 @@ static int handle_series_get(struct bt_mesh_model *model, struct bt_mesh_msg_ctx
 
 	/* Check buf->len different from 0, before decoding buf to range and buf->len changes. */
 	bool ranged = (buf->len != 0);
-	struct bt_mesh_sensor_column range;
+	sensor_value_type range_start, range_end;
 
 	if (buf->len == col_format->size * 2) {
 		int err;
 
-		err = sensor_ch_decode(buf, col_format, &range.start);
+		err = sensor_ch_decode(buf, col_format, &range_start);
 		if (err) {
 			LOG_WRN("Range start decode failed: %d", err);
 			return err;
 		}
 
-		err = sensor_ch_decode(buf, col_format, &range.end);
+		err = sensor_ch_decode(buf, col_format, &range_end);
 		if (err) {
 			LOG_WRN("Range end decode failed: %d", err);
 			return err;
@@ -430,7 +434,7 @@ static int handle_series_get(struct bt_mesh_model *model, struct bt_mesh_msg_ctx
 			&sensor->series.columns[i];
 
 		if (ranged &&
-		    !bt_mesh_sensor_value_in_column(&col->start, &range)) {
+		    !SENSOR_VALUE_IN_RANGE(&col->start, &range_start, &range_end)) {
 			continue;
 		}
 
@@ -553,8 +557,13 @@ static int cadence_set(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx,
 		char range_low_str[BT_MESH_SENSOR_CH_STR_LEN];
 		char range_high_str[BT_MESH_SENSOR_CH_STR_LEN];
 
+#ifdef CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE
 		strcpy(delta_down_str, bt_mesh_sensor_ch_str(&threshold.delta.down));
 		strcpy(delta_up_str, bt_mesh_sensor_ch_str(&threshold.delta.up));
+#else
+		strcpy(delta_down_str, bt_mesh_sensor_ch_str(&threshold.deltas.down));
+		strcpy(delta_up_str, bt_mesh_sensor_ch_str(&threshold.deltas.up));
+#endif
 		strcpy(range_low_str, bt_mesh_sensor_ch_str(&threshold.range.low));
 		strcpy(range_high_str, bt_mesh_sensor_ch_str(&threshold.range.high));
 
@@ -696,7 +705,7 @@ static int handle_setting_get(struct bt_mesh_model *model, struct bt_mesh_msg_ct
 
 	net_buf_simple_add_u8(&rsp, setting->set ? 0x03 : 0x01);
 
-	struct sensor_value values[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = { 0 };
+	sensor_value_type values[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = { 0 };
 
 	setting->get(srv, sensor, setting, ctx, values);
 
@@ -747,7 +756,7 @@ static int setting_set(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx,
 		goto respond;
 	}
 
-	struct sensor_value values[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX];
+	sensor_value_type values[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX];
 
 	err = sensor_value_decode(buf, setting->type, values);
 	if (err) {
@@ -904,7 +913,7 @@ static void pub_msg_add(struct bt_mesh_sensor_srv *srv,
 		return;
 	}
 
-	struct sensor_value value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
+	sensor_value_type value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
 
 	err = value_get(srv, s, NULL, value);
 	if (err) {
@@ -1142,7 +1151,11 @@ const struct bt_mesh_model_cb _bt_mesh_sensor_setup_srv_cb = {
 int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
 			   struct bt_mesh_msg_ctx *ctx,
 			   struct bt_mesh_sensor *sensor,
+#ifdef CONFIG_BT_MESH_SENSOR_USE_SENSOR_VALUE
 			   const struct sensor_value *value)
+#else
+			   const struct bt_mesh_sensor_value *value)
+#endif
 {
 	int err;
 
@@ -1169,7 +1182,7 @@ int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
 int bt_mesh_sensor_srv_sample(struct bt_mesh_sensor_srv *srv,
 			      struct bt_mesh_sensor *sensor)
 {
-	struct sensor_value value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
+	sensor_value_type value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
 	int err;
 
 	err = value_get(srv, sensor, NULL, value);

Sandy6Zhou/nrf

include/bluetooth/mesh/sensor_srv.h
@@ -94,6 +94,30 @@ struct bt_mesh_sensor_srv {
 	struct bt_mesh_model *model;
 };
 
+#if !defined(CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE) || defined(__DOXYGEN__)
+/** @brief Publish a sensor value.
+ *
+ *  Immediately publishes the given sensor value, without checking thresholds
+ *  or intervals.
+ *
+ *  @see bt_mesh_sensor_srv_pub
+ *
+ *  @param[in] srv    Sensor server instance.
+ *  @param[in] ctx    Message context to publish with, or NULL to publish on the
+ *                    configured publish parameters.
+ *  @param[in] sensor Sensor to publish with.
+ *  @param[in] value  Sensor value to publish, interpreted as an array of sensor
+ *                    channel values matching the sensor channels specified by
+ *                    the sensor type. The length of the array must match the
+ *                    sensor channel count.
+ *
+ *  @return 0 on success, or (negative) error code otherwise.
+ */
+int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
+			   struct bt_mesh_msg_ctx *ctx,
+			   struct bt_mesh_sensor *sensor,
+			   const struct bt_mesh_sensor_value *value);
+#else /* defined(CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE) */
 /** @brief Publish a sensor value.
  *
  *  Immediately publishes the given sensor value, without checking thresholds
@@ -116,7 +140,7 @@ int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
 			   struct bt_mesh_msg_ctx *ctx,
 			   struct bt_mesh_sensor *sensor,
 			   const struct sensor_value *value);
-
+#endif /* !defined(CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE) */
 
 /** @brief Make the server to take a sample of the sensor, and publish if the
  *         value changed sufficiently.

subsys/bluetooth/mesh/sensor.h
@@ -130,9 +130,55 @@ extern const struct bt_mesh_sensor_unit bt_mesh_sensor_unit_unitless;
 
 /** @} */
 
+/* Temporary typedef while we need to support two different sensor value types
+ * in the deprecation period. Can be removed once support for
+ * struct sensor_value is removed, and internal APIs changed to use
+ * struct bt_mesh_sensor_value.
+ */
+#ifdef CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE
+typedef struct sensor_value sensor_value_type;
+#else
+typedef struct bt_mesh_sensor_value sensor_value_type;
+#endif
+
+#ifdef CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE
+#define SENSOR_VALUE_IN_RANGE(_value, _start, _end) (                          \
+		((_value)->val1 > (_start)->val1 ||                            \
+		 ((_value)->val1 == (_start)->val1 &&                          \
+		  (_value)->val2 >= (_start)->val2)) &&                        \
+		((_value)->val1 < (_end)->val1 ||                              \
+		 ((_value)->val1 == (_end)->val1 &&                            \
+		  (_value)->val2 <= (_end)->val2)))
+#else
+#define SENSOR_VALUE_IN_RANGE(_value, _start, _end) (                          \
+		(_value)->format->compare((_value), (_start)) >= 0 &&          \
+		(_value)->format->compare((_end), (_value)) >= 0)
+
+/** @brief Check if a value change breaks the delta threshold.
+ *
+ *  Sensors should publish their value if the measured sample is outside the
+ *  delta threshold compared to the previously published value. This function
+ *  checks the threshold and the previously published value for this sensor,
+ *  and returns whether the sensor should publish its value.
+ *
+ *  @note Only single-channel sensors support cadence. Multi-channel sensors are
+ *        always considered out of their threshold range, and will always return
+ *        true from this function. Single-channel sensors that haven't been
+ *        assigned a threshold will return true if the value is different.
+ *
+ *  @param[in] sensor    The sensor instance.
+ *  @param[in] value     Sensor value.
+ *
+ *  @return true if the difference between the measurements exceeds the delta
+ *          threshold, false otherwise.
+ */
+bool bt_mesh_sensor_delta_threshold(const struct bt_mesh_sensor *sensor,
+				    const struct bt_mesh_sensor_value *value);
+#endif /* CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE */
+
 int sensor_status_encode(struct net_buf_simple *buf,
 			 const struct bt_mesh_sensor *sensor,
-			 const struct sensor_value *values);
+			 const sensor_value_type *values);
 
 int sensor_status_id_encode(struct net_buf_simple *buf, uint8_t len, uint16_t id);
 void sensor_status_id_decode(struct net_buf_simple *buf, uint8_t *len, uint16_t *id);
@@ -144,17 +190,17 @@ void sensor_descriptor_encode(struct net_buf_simple *buf,
 
 int sensor_value_encode(struct net_buf_simple *buf,
 			const struct bt_mesh_sensor_type *type,
-			const struct sensor_value *values);
+			const sensor_value_type *values);
 int sensor_value_decode(struct net_buf_simple *buf,
 			const struct bt_mesh_sensor_type *type,
-			struct sensor_value *values);
+			sensor_value_type *values);
 
 int sensor_ch_encode(struct net_buf_simple *buf,
 		     const struct bt_mesh_sensor_format *format,
-		     const struct sensor_value *value);
+		     const sensor_value_type *value);
 int sensor_ch_decode(struct net_buf_simple *buf,
 		     const struct bt_mesh_sensor_format *format,
-		     struct sensor_value *value);
+		     sensor_value_type *value);
 
 int sensor_column_value_encode(struct net_buf_simple *buf,
 			       struct bt_mesh_sensor_srv *srv,
@@ -169,7 +215,7 @@ int sensor_column_encode(struct net_buf_simple *buf,
 int sensor_column_decode(
 	struct net_buf_simple *buf, const struct bt_mesh_sensor_type *type,
 	struct bt_mesh_sensor_column *col,
-	struct sensor_value value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX]);
+	sensor_value_type value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX]);
 
 int sensor_cadence_encode(struct net_buf_simple *buf,
 			  const struct bt_mesh_sensor_type *sensor_type,
@@ -188,7 +234,7 @@ uint64_t sensor_powtime_decode_us(uint8_t val);
 uint8_t sensor_pub_div_get(const struct bt_mesh_sensor *s, uint32_t base_period);
 
 void sensor_cadence_update(struct bt_mesh_sensor *sensor,
-			   const struct sensor_value *value);
+			   const sensor_value_type *value);
 
 #ifdef __cplusplus
 }

subsys/bluetooth/mesh/sensor_srv.c
@@ -80,7 +80,7 @@ static void cadence_store(struct bt_mesh_sensor_srv *srv)
 static int value_get(struct bt_mesh_sensor_srv *srv,
 		     struct bt_mesh_sensor *sensor,
 		     struct bt_mesh_msg_ctx *ctx,
-		     struct sensor_value *value)
+		     sensor_value_type *value)
 {
 	int err;
 
@@ -104,7 +104,7 @@ static int buf_status_add(struct bt_mesh_sensor_srv *srv,
 			  struct bt_mesh_msg_ctx *ctx,
 			  struct net_buf_simple *buf)
 {
-	struct sensor_value value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
+	sensor_value_type value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
 	struct net_buf_simple_state state;
 	int err;
 
@@ -223,11 +223,15 @@ respond:
 
 static const struct bt_mesh_sensor_column *
 column_get(const struct bt_mesh_sensor_series *series,
-	   const struct sensor_value *val)
+	   const sensor_value_type *val)
 {
 	for (uint32_t i = 0; i < series->column_count; ++i) {
+#ifdef CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE
 		if (series->columns[i].start.val1 == val->val1 &&
 		    series->columns[i].start.val2 == val->val2) {
+#else
+		if (val->format->compare(&series->columns[i].start, val) == 0) {
+#endif
 			return &series->columns[i];
 		}
 	}
@@ -287,7 +291,7 @@ static int handle_column_get(struct bt_mesh_model *model, struct bt_mesh_msg_ctx
 
 	const struct bt_mesh_sensor_format *col_format;
 	const struct bt_mesh_sensor_column *col;
-	struct sensor_value col_x;
+	sensor_value_type col_x;
 
 	col_format = bt_mesh_sensor_column_format_get(sensor->type);
 
@@ -403,18 +407,18 @@ static int handle_series_get(struct bt_mesh_model *model, struct bt_mesh_msg_ctx
 
 	/* Check buf->len different from 0, before decoding buf to range and buf->len changes. */
 	bool ranged = (buf->len != 0);
-	struct bt_mesh_sensor_column range;
+	sensor_value_type range_start, range_end;
 
 	if (buf->len == col_format->size * 2) {
 		int err;
 
-		err = sensor_ch_decode(buf, col_format, &range.start);
+		err = sensor_ch_decode(buf, col_format, &range_start);
 		if (err) {
 			LOG_WRN("Range start decode failed: %d", err);
 			return err;
 		}
 
-		err = sensor_ch_decode(buf, col_format, &range.end);
+		err = sensor_ch_decode(buf, col_format, &range_end);
 		if (err) {
 			LOG_WRN("Range end decode failed: %d", err);
 			return err;
@@ -430,7 +434,7 @@ static int handle_series_get(struct bt_mesh_model *model, struct bt_mesh_msg_ctx
 			&sensor->series.columns[i];
 
 		if (ranged &&
-		    !bt_mesh_sensor_value_in_column(&col->start, &range)) {
+		    !SENSOR_VALUE_IN_RANGE(&col->start, &range_start, &range_end)) {
 			continue;
 		}
 
@@ -553,8 +557,13 @@ static int cadence_set(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx,
 		char range_low_str[BT_MESH_SENSOR_CH_STR_LEN];
 		char range_high_str[BT_MESH_SENSOR_CH_STR_LEN];
 
+#ifdef CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE
 		strcpy(delta_down_str, bt_mesh_sensor_ch_str(&threshold.delta.down));
 		strcpy(delta_up_str, bt_mesh_sensor_ch_str(&threshold.delta.up));
+#else
+		strcpy(delta_down_str, bt_mesh_sensor_ch_str(&threshold.deltas.down));
+		strcpy(delta_up_str, bt_mesh_sensor_ch_str(&threshold.deltas.up));
+#endif
 		strcpy(range_low_str, bt_mesh_sensor_ch_str(&threshold.range.low));
 		strcpy(range_high_str, bt_mesh_sensor_ch_str(&threshold.range.high));
 
@@ -696,7 +705,7 @@ static int handle_setting_get(struct bt_mesh_model *model, struct bt_mesh_msg_ct
 
 	net_buf_simple_add_u8(&rsp, setting->set ? 0x03 : 0x01);
 
-	struct sensor_value values[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = { 0 };
+	sensor_value_type values[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = { 0 };
 
 	setting->get(srv, sensor, setting, ctx, values);
 
@@ -747,7 +756,7 @@ static int setting_set(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx,
 		goto respond;
 	}
 
-	struct sensor_value values[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX];
+	sensor_value_type values[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX];
 
 	err = sensor_value_decode(buf, setting->type, values);
 	if (err) {
@@ -904,7 +913,7 @@ static void pub_msg_add(struct bt_mesh_sensor_srv *srv,
 		return;
 	}
 
-	struct sensor_value value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
+	sensor_value_type value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
 
 	err = value_get(srv, s, NULL, value);
 	if (err) {
@@ -1142,7 +1151,11 @@ const struct bt_mesh_model_cb _bt_mesh_sensor_setup_srv_cb = {
 int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
 			   struct bt_mesh_msg_ctx *ctx,
 			   struct bt_mesh_sensor *sensor,
+#ifdef CONFIG_BT_MESH_SENSOR_USE_LEGACY_SENSOR_VALUE
 			   const struct sensor_value *value)
+#else
+			   const struct bt_mesh_sensor_value *value)
+#endif
 {
 	int err;
 
@@ -1169,7 +1182,7 @@ int bt_mesh_sensor_srv_pub(struct bt_mesh_sensor_srv *srv,
 int bt_mesh_sensor_srv_sample(struct bt_mesh_sensor_srv *srv,
 			      struct bt_mesh_sensor *sensor)
 {
-	struct sensor_value value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
+	sensor_value_type value[CONFIG_BT_MESH_SENSOR_CHANNELS_MAX] = {};
 	int err;
 
 	err = value_get(srv, sensor, NULL, value);