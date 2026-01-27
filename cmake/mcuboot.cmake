sigvartmh/fw-nrfconnect-nrf-1

cmake: mcuboot: Add signing of network core update
Make the build system sign network core updates with MCUBoot
header/trailer so that they can be used and validated by MCUBoot.

Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

cmake/mcuboot.cmake
@@ -181,6 +181,36 @@ if(CONFIG_BOOTLOADER_MCUBOOT)
     "version_MCUBOOT=${CONFIG_MCUBOOT_IMAGE_VERSION}"
     )
 
+  if (CONFIG_BT_RPMSG_NRF53 AND CONFIG_HCI_RPMSG_BUILD_STRATEGY_FROM_SOURCE
+      AND CONFIG_SOC_NRF5340_CPUAPP)
+    # Network core application updates are enabled.
+    # We know this since MCUBoot is enabled on the application core, and
+    # a network core child image is included in the build.
+    # These updates are verified by the application core MCUBoot.
+    # Create a signed variant of the network core application.
+
+    # Load the shared vars to get the path to the hex file to sign.
+    include(${CMAKE_BINARY_DIR}/hci_rpmsg/shared_vars.cmake)
+
+    sign(${CPUNET_PM_APP_HEX}
+      ${PROJECT_BINARY_DIR}/net_core_app
+      $<TARGET_PROPERTY:partition_manager,net_app_TO_SECONDARY>
+      hci_rpmsg_subimage
+      net_core_app_signed_hex
+      )
+
+    add_custom_target(
+      net_core_app_sign_target
+      DEPENDS ${net_core_app_signed_hex}
+      )
+
+    add_dependencies(
+      mcuboot_sign_target
+      net_core_app_sign_target
+      )
+
+  endif()
+
   if (CONFIG_BUILD_S1_VARIANT AND ("${CONFIG_S1_VARIANT_IMAGE_NAME}" STREQUAL "mcuboot"))
     # Secure Boot (B0) is enabled, and we have to build update candidates
     # for both S1 and S0.

cmake/partition_manager.cmake
@@ -350,6 +350,8 @@ if (is_dynamic_partition_in_domain)
   share("set(${DOMAIN}_PM_DOMAIN_HEADER_FILES ${header_files})")
   share("set(${DOMAIN}_PM_DOMAIN_IMAGES ${prefixed_images})")
   share("set(${DOMAIN}_PM_HEX_FILE ${PROJECT_BINARY_DIR}/${merged}.hex)")
+  share("set(${DOMAIN}_PM_DOTCONF_FILES ${pm_out_dotconf_files})")
+  share("set(${DOMAIN}_PM_APP_HEX ${PROJECT_BINARY_DIR}/app.hex)")
 else()
   # This is the root image, generate the global pm_config.h
   # First, include the shared_vars.cmake file for all child images.
@@ -374,6 +376,51 @@ else()
       list(APPEND pm_out_region_files ${${d}_PM_DOMAIN_REGIONS})
       list(APPEND global_hex_depends ${${d}_PM_DOMAIN_DYNAMIC_PARTITION}_subimage)
       list(APPEND domain_hex_files ${${d}_PM_HEX_FILE})
+
+      # Add domain prefix cmake variables for all partitions
+      # Here, we actually overwrite the already imported kconfig values
+      # for our own domain. This is not an issue since all of these variables
+      # are accessed through the 'partition_manager' target, and most likely
+      # through generator expression, as this file is one of the last
+      # cmake files executed in the configure stage.
+      import_kconfig(PM_ ${${d}_PM_DOTCONF_FILES} ${d}_pm_var_names)
+
+      foreach(name ${${d}_pm_var_names})
+        set_property(
+          TARGET partition_manager
+          PROPERTY ${d}_${name}
+          ${${name}}
+          )
+      endforeach()
+
+      if (CONFIG_BT_RPMSG_NRF53
+          AND CONFIG_BOOTLOADER_MCUBOOT
+          AND CONFIG_SOC_NRF5340_CPUAPP
+          AND CONFIG_HCI_RPMSG_BUILD_STRATEGY_FROM_SOURCE)
+          # Create symbols for the offset reqired for moving the signed network
+          # core application to MCUBoots secondary slot. This is needed
+          # because  objcopy does not support arithmetic expressions as argument
+          # (e.g. '0x100+0x200'), and all of the symbols used to generate the
+          # offset is only available as a generator expression when MCUBoots
+          # cmake code exectues.
+
+          get_target_property(
+            net_app_addr
+            partition_manager
+            CPUNET_PM_APP_ADDRESS
+            )
+
+          # There is no padding in front of the network core application.
+          math(EXPR net_app_TO_SECONDARY
+            "${PM_MCUBOOT_SECONDARY_ADDRESS} - ${net_app_addr} + ${PM_MCUBOOT_PAD_SIZE}")
+
+          set_property(
+            TARGET partition_manager
+            PROPERTY net_app_TO_SECONDARY
+            ${net_app_TO_SECONDARY}
+            )
+        endif()
+
     endif()
   endforeach()
 

Sandy6Zhou/nrf

Ref: NCSDK-4705

cmake/mcuboot.cmake
@@ -181,6 +181,36 @@ if(CONFIG_BOOTLOADER_MCUBOOT)
     "version_MCUBOOT=${CONFIG_MCUBOOT_IMAGE_VERSION}"
     )
 
+  if (CONFIG_BT_RPMSG_NRF53 AND CONFIG_HCI_RPMSG_BUILD_STRATEGY_FROM_SOURCE
+      AND CONFIG_SOC_NRF5340_CPUAPP)
+    # Network core application updates are enabled.
+    # We know this since MCUBoot is enabled on the application core, and
+    # a network core child image is included in the build.
+    # These updates are verified by the application core MCUBoot.
+    # Create a signed variant of the network core application.
+
+    # Load the shared vars to get the path to the hex file to sign.
+    include(${CMAKE_BINARY_DIR}/hci_rpmsg/shared_vars.cmake)
+
+    sign(${CPUNET_PM_SIGNED_APP_HEX}
+      ${PROJECT_BINARY_DIR}/net_core_app
+      $<TARGET_PROPERTY:partition_manager,net_app_TO_SECONDARY>
+      hci_rpmsg_subimage
+      net_core_app_signed_hex
+      )
+
+    add_custom_target(
+      net_core_app_sign_target
+      DEPENDS ${net_core_app_signed_hex}
+      )
+
+    add_dependencies(
+      mcuboot_sign_target
+      net_core_app_sign_target
+      )
+
+  endif()
+
   if (CONFIG_BUILD_S1_VARIANT AND ("${CONFIG_S1_VARIANT_IMAGE_NAME}" STREQUAL "mcuboot"))
     # Secure Boot (B0) is enabled, and we have to build update candidates
     # for both S1 and S0.

cmake/partition_manager.cmake
@@ -373,6 +373,9 @@ if (is_dynamic_partition_in_domain)
   share("set(${DOMAIN}_PM_DOMAIN_HEADER_FILES ${header_files})")
   share("set(${DOMAIN}_PM_DOMAIN_IMAGES ${prefixed_images})")
   share("set(${DOMAIN}_PM_HEX_FILE ${PROJECT_BINARY_DIR}/${merged}.hex)")
+  share("set(${DOMAIN}_PM_DOTCONF_FILES ${pm_out_dotconf_file})")
+  share("set(${DOMAIN}_PM_APP_HEX ${PROJECT_BINARY_DIR}/app.hex)")
+  share("set(${DOMAIN}_PM_SIGNED_APP_HEX ${PROJECT_BINARY_DIR}/signed_by_b0_app.hex)")
 else()
   # This is the root image, generate the global pm_config.h
   # First, include the shared_vars.cmake file for all child images.
@@ -397,6 +400,51 @@ else()
       list(APPEND pm_out_region_file ${${d}_PM_DOMAIN_REGIONS})
       list(APPEND global_hex_depends ${${d}_PM_DOMAIN_DYNAMIC_PARTITION}_subimage)
       list(APPEND domain_hex_files ${${d}_PM_HEX_FILE})
+
+      # Add domain prefix cmake variables for all partitions
+      # Here, we actually overwrite the already imported kconfig values
+      # for our own domain. This is not an issue since all of these variables
+      # are accessed through the 'partition_manager' target, and most likely
+      # through generator expression, as this file is one of the last
+      # cmake files executed in the configure stage.
+      import_kconfig(PM_ ${${d}_PM_DOTCONF_FILES} ${d}_pm_var_names)
+
+      foreach(name ${${d}_pm_var_names})
+        set_property(
+          TARGET partition_manager
+          PROPERTY ${d}_${name}
+          ${${name}}
+          )
+      endforeach()
+
+      if (CONFIG_BT_RPMSG_NRF53
+          AND CONFIG_BOOTLOADER_MCUBOOT
+          AND CONFIG_SOC_NRF5340_CPUAPP
+          AND CONFIG_HCI_RPMSG_BUILD_STRATEGY_FROM_SOURCE)
+          # Create symbols for the offset reqired for moving the signed network
+          # core application to MCUBoots secondary slot. This is needed
+          # because  objcopy does not support arithmetic expressions as argument
+          # (e.g. '0x100+0x200'), and all of the symbols used to generate the
+          # offset are only available as a generator expression when MCUBoots
+          # cmake code exectues.
+
+          get_target_property(
+            net_app_addr
+            partition_manager
+            CPUNET_PM_APP_ADDRESS
+            )
+
+          # There is no padding in front of the network core application.
+          math(EXPR net_app_TO_SECONDARY
+            "${PM_MCUBOOT_SECONDARY_ADDRESS} - ${net_app_addr} + ${PM_MCUBOOT_PAD_SIZE}")
+
+          set_property(
+            TARGET partition_manager
+            PROPERTY net_app_TO_SECONDARY
+            ${net_app_TO_SECONDARY}
+            )
+        endif()
+
     endif()
   endforeach()
 