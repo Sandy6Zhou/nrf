nrfconnect/sdk-nrf

samples: nrf5340: Move and rename empty_net_core project
Moves this project to a new folder - multicore, as this (simple)
project can be built for many other multi-core CPUs. Also updates
sysbuild configuration to use the new folder location

Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

doc/_utils/redirects.py
@@ -446,6 +446,7 @@ NRF = (
     ("samples/samples_other", "samples/other"), # Other samples (landing)
     ("samples/pmic/native/npm1300_fuel_gauge/README", "samples/pmic/native/npm13xx_fuel_gauge/README"),
     ("samples/pmic/native/npm1300_one_button/README", "samples/pmic/native/npm13xx_one_button/README"),
+    ("samples/nrf5340/empty_network_core/README", "samples/multicore/empty/README"),
     ("libraries/bluetooth_services/index", "libraries/bluetooth/index"), # Changed the folder name libraries/bluetooth_services to libraries/bluetooth to better match the include folder structure
     ("libraries/bluetooth_services/adv_prov", "libraries/bluetooth/adv_prov"),
     ("libraries/bluetooth_services/conn_ctx", "libraries/bluetooth/conn_ctx"),

Sandy6Zhou/nrf

doc/_utils/redirects.py
@@ -447,6 +447,7 @@ NRF = (
     ("samples/samples_other", "samples/other"), # Other samples (landing)
     ("samples/pmic/native/npm1300_fuel_gauge/README", "samples/pmic/native/npm13xx_fuel_gauge/README"),
     ("samples/pmic/native/npm1300_one_button/README", "samples/pmic/native/npm13xx_one_button/README"),
+    ("samples/nrf5340/empty_network_core/README", "samples/basic/empty/README"),
     ("libraries/bluetooth_services/index", "libraries/bluetooth/index"), # Changed the folder name libraries/bluetooth_services to libraries/bluetooth to better match the include folder structure
     ("libraries/bluetooth_services/adv_prov", "libraries/bluetooth/adv_prov"),
     ("libraries/bluetooth_services/conn_ctx", "libraries/bluetooth/conn_ctx"),