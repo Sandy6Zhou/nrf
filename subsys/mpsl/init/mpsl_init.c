rick1082/sdk-nrf

applications: nrf5340_audio: Support time sync using RTC
Some controllers base their system clock upon an RTC.
The implementation is changed as follows:

- The RTC is started on app and network core at the same time
  using the DPPI channel `dppi_channel_rtc_start` on the app core.
- The sync timer is cleared at every RTC tick.
- The audio_sync_timer_capture() now correponds to RTC time + TIMER0
  time.

Co-authored-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

Signed-off-by: Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>

subsys/mpsl/init/mpsl_init.c
@@ -17,6 +17,9 @@
 #if defined(CONFIG_NRFX_DPPI)
 #include <nrfx_dppi.h>
 #endif
+#if defined(CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START_CHANNEL)
+#include <hal/nrf_ipc.h>
+#endif
 
 LOG_MODULE_REGISTER(mpsl_init, CONFIG_MPSL_LOG_LEVEL);
 
@@ -225,6 +228,14 @@ static int32_t mpsl_lib_init_internal(void)
 {
 	int err = 0;
 	mpsl_clock_lfclk_cfg_t clock_cfg;
+
+#ifdef CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START_CHANNEL
+	nrf_ipc_send_config_set(NRF_IPC,
+		CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START_CHANNEL,
+		(1UL << CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START_CHANNEL));
+	mpsl_clock_task_trigger_on_rtc_start_set(
+		(uint32_t)&NRF_IPC->TASKS_SEND[CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START_CHANNEL]);
+#endif
 
 	clock_cfg.source = m_config_clock_source_get();
 	clock_cfg.accuracy_ppm = CONFIG_CLOCK_CONTROL_NRF_ACCURACY;

Sandy6Zhou/nrf

subsys/mpsl/init/mpsl_init.c
@@ -17,6 +17,9 @@
 #if defined(CONFIG_NRFX_DPPI)
 #include <nrfx_dppi.h>
 #endif
+#if defined(CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START)
+#include <hal/nrf_ipc.h>
+#endif
 
 LOG_MODULE_REGISTER(mpsl_init, CONFIG_MPSL_LOG_LEVEL);
 
@@ -225,6 +228,14 @@ static int32_t mpsl_lib_init_internal(void)
 {
 	int err = 0;
 	mpsl_clock_lfclk_cfg_t clock_cfg;
+
+#ifdef CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START
+	nrf_ipc_send_config_set(NRF_IPC,
+		CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START_CHANNEL,
+		(1UL << CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START_CHANNEL));
+	mpsl_clock_task_trigger_on_rtc_start_set(
+		(uint32_t)&NRF_IPC->TASKS_SEND[CONFIG_MPSL_TRIGGER_IPC_TASK_ON_RTC_START_CHANNEL]);
+#endif
 
 	clock_cfg.source = m_config_clock_source_get();
 	clock_cfg.accuracy_ppm = CONFIG_CLOCK_CONTROL_NRF_ACCURACY;