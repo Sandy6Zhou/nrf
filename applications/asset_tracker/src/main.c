jayteemo/fw-nrfconnect-nrf

applications: asset_tracker: changes for new nrf_cloud_fota
Some minor changes required for using nrf_cloud_fota.
nRF Cloud will no longer be supporting aws_fota (fota_v1).

Signed-off-by: Justin Morton <justin.morton@nordicsemi.no>

applications/asset_tracker/src/main.c
@@ -1377,7 +1377,7 @@ void cloud_event_handler(const struct cloud_backend *const backend,
 		LOG_INF("CLOUD_EVT_READY");
 		ui_led_set_pattern(UI_CLOUD_CONNECTED);
 
-#if defined(CONFIG_BOOTLOADER_MCUBOOT)
+#if defined(CONFIG_BOOTLOADER_MCUBOOT) && !IS_ENABLED(CONFIG_NRF_CLOUD_FOTA)
 		/* Mark image as good to avoid rolling back after update */
 		boot_write_img_confirmed();
 #endif
@@ -1750,7 +1750,7 @@ static void ui_evt_handler(struct ui_evt evt)
 
 void handle_bsdlib_init_ret(void)
 {
-#if defined(CONFIG_BSD_LIBRARY)
+#if defined(CONFIG_BSD_LIBRARY) && !IS_ENABLED(CONFIG_NRF_CLOUD_FOTA)
 	int ret = bsdlib_get_init_ret();
 
 	/* Handle return values relating to modem firmware update */

Sandy6Zhou/nrf

applications/asset_tracker/src/main.c
@@ -1404,7 +1404,7 @@ void cloud_event_handler(const struct cloud_backend *const backend,
 		LOG_INF("CLOUD_EVT_READY");
 		ui_led_set_pattern(UI_CLOUD_CONNECTED);
 
-#if defined(CONFIG_BOOTLOADER_MCUBOOT)
+#if defined(CONFIG_BOOTLOADER_MCUBOOT) && !defined(CONFIG_NRF_CLOUD_FOTA)
 		/* Mark image as good to avoid rolling back after update */
 		boot_write_img_confirmed();
 #endif
@@ -1790,7 +1790,7 @@ static void ui_evt_handler(struct ui_evt evt)
 
 void handle_nrf_modem_lib_init_ret(void)
 {
-#if defined(CONFIG_NRF_MODEM_LIB)
+#if defined(CONFIG_NRF_MODEM_LIB) && !defined(CONFIG_NRF_CLOUD_FOTA)
 	int ret = nrf_modem_lib_get_init_ret();
 
 	/* Handle return values relating to modem firmware update */