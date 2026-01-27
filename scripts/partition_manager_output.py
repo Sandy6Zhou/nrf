nrfconnect/sdk-nrf

partition_manager: generalize regions
In order to simplify extending partition manager
with other regions than internal flash, generalize
the input to the partition manager so that regions
are provided as part of the configuration.

Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

scripts/partition_manager_output.py
@@ -24,7 +24,8 @@ def get_header_guard_end(filename):
 DEST_HEADER = 1
 DEST_KCONFIG = 2
 
-def get_config_lines(pm_config, head, split, dest):
+
+def get_config_lines(pm_config, regions_config, head, split, dest):
     config_lines = list()
 
     def add_line(a, b):
@@ -33,32 +34,40 @@ def get_config_lines(pm_config, head, split, dest):
     def string_of_strings(mlist):
         return '"%s"' % " ".join(["%s" % elem for elem in mlist])
 
+    def partition_has_device(p):
+        return 'device' in regions_config[p['region']] and regions_config[p['region']]['device']
+
     partition_id = 0
-    for name, partition in sorted(pm_config.items(), key=lambda key_value_tuple: key_value_tuple[1]['address']):
+    for name, partition in sorted(pm_config.items(),
+                                  key=lambda key_value_tuple: key_value_tuple[1]['address']):
+
         add_line("%s_ADDRESS" % name.upper(), "0x%x" % partition['address'])
         add_line("%s_SIZE" % name.upper(), "0x%x" % partition['size'])
-        add_line("%s_ID" % name.upper(), "%d" % partition_id)
         add_line("%s_NAME" % name.upper(), "%s" % name)
-        add_line("%d_LABEL" % partition_id, "%s" % name.upper())
+
+        if partition_has_device(partition):
+            add_line("%s_ID" % name.upper(), "%d" % partition_id)
+            add_line("%d_LABEL" % partition_id, "%s" % name.upper())
+            partition_id += 1
+
         if dest is DEST_HEADER:
-            add_line("%s_DEV_NAME" % name.upper(), "\"NRF_FLASH_DRV_NAME\"")
+            if partition_has_device(partition):
+                add_line("%s_DEV_NAME" % name.upper(), f"\"{regions_config[partition['region']]['device']}\"")
         elif dest is DEST_KCONFIG:
             if 'span' in partition.keys():
                 add_line("%s_SPAN" % name.upper(), string_of_strings(partition['span']))
 
-        pm_config[name]['partition_id'] = partition_id
-        partition_id += 1
     add_line("NUM", "%d" % partition_id)
 
-    def find_depth(key, depth = 0):
+    def find_depth(key, depth=0):
         if 'span' in pm_config[key].keys():
             return find_depth(pm_config[key]['span'][0], depth + 1)
         return depth
 
-    all_by_size = list(pm_config.keys())
-
+    flash_partition_pm_config = {k: v for k, v in pm_config.items() if partition_has_device(k)}
+    all_by_size = list(flash_partition_pm_config.keys())
     all_by_size = sorted(all_by_size, key=find_depth)
-    all_by_size = sorted(all_by_size, key=lambda key: pm_config[key]['size'])
+    all_by_size = sorted(all_by_size, key=lambda key: flash_partition_pm_config[key]['size'])
     add_line("ALL_BY_SIZE", string_of_strings(all_by_size))
 
     return config_lines
@@ -69,9 +78,9 @@ def write_config_lines_to_file(pm_config_file_path, config_lines):
         out_file.write('\n'.join(config_lines))
 
 
-def write_pm_config(pm_config, name, out_path):
+def write_pm_config(pm_config, regions_config, name, out_path):
     pm_config_file = path.basename(out_path)
-    config_lines = get_config_lines(pm_config, "#define ", " ", DEST_HEADER)
+    config_lines = get_config_lines(pm_config, regions_config, "#define ", " ", DEST_HEADER)
 
     image_config_lines = list.copy(config_lines)
     image_config_lines.append("#define PM_ADDRESS 0x%x" % pm_config[name]['address'])
@@ -81,8 +90,8 @@ def write_pm_config(pm_config, name, out_path):
     write_config_lines_to_file(out_path, image_config_lines)
 
 
-def write_kconfig_file(pm_config, out_path):
-    config_lines = get_config_lines(pm_config, "", "=", DEST_KCONFIG)
+def write_kconfig_file(pm_config, regions_config, out_path):
+    config_lines = get_config_lines(pm_config, regions_config, "", "=", DEST_KCONFIG)
     write_config_lines_to_file(out_path, config_lines)
 
 
@@ -91,8 +100,11 @@ def parse_args():
         description='''Creates files based on Partition Manager results.''',
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument("--input", required=True, type=str,
-                        help="Path to the input .yml file.")
+    parser.add_argument("--input-partitions", required=True, type=str,
+                        help="Path to the input .yml file with partition configuration.")
+
+    parser.add_argument("--input-regions", required=True, type=str,
+                        help="Path to the input .yml file with region configuration.")
 
     parser.add_argument("--config-file", required=True, type=str,
                         help="Path to the output .config file.")
@@ -110,13 +122,15 @@ def parse_args():
 def main():
     args = parse_args()
 
-    with open(args.input, 'r') as f:
+    with open(args.input_partitions, 'r') as f:
         pm_config = yaml.safe_load(f)
+    with open(args.input_regions, 'r') as f:
+        regions_config = yaml.safe_load(f)
 
-    write_kconfig_file(pm_config, args.config_file)
+    write_kconfig_file(pm_config, regions_config, args.config_file)
 
     for name, header_file in zip(args.input_names, args.header_files):
-        write_pm_config(pm_config, name, header_file)
+        write_pm_config(pm_config, regions_config, name, header_file)
 
 
 if __name__ == "__main__":

Sandy6Zhou/nrf

scripts/partition_manager_output.py
@@ -24,7 +24,8 @@ def get_header_guard_end(filename):
 DEST_HEADER = 1
 DEST_KCONFIG = 2
 
-def get_config_lines(pm_config, head, split, dest):
+
+def get_config_lines(pm_config, regions_config, head, split, dest):
     config_lines = list()
 
     def add_line(a, b):
@@ -33,32 +34,40 @@ def get_config_lines(pm_config, head, split, dest):
     def string_of_strings(mlist):
         return '"%s"' % " ".join(["%s" % elem for elem in mlist])
 
+    def partition_has_device(p):
+        return 'region' in p and 'device' in regions_config[p['region']] and regions_config[p['region']]['device']
+
     partition_id = 0
-    for name, partition in sorted(pm_config.items(), key=lambda key_value_tuple: key_value_tuple[1]['address']):
+    for name, partition in sorted(pm_config.items(),
+                                  key=lambda key_value_tuple: key_value_tuple[1]['address']):
+
         add_line("%s_ADDRESS" % name.upper(), "0x%x" % partition['address'])
         add_line("%s_SIZE" % name.upper(), "0x%x" % partition['size'])
-        add_line("%s_ID" % name.upper(), "%d" % partition_id)
         add_line("%s_NAME" % name.upper(), "%s" % name)
-        add_line("%d_LABEL" % partition_id, "%s" % name.upper())
+
+        if partition_has_device(partition):
+            add_line("%s_ID" % name.upper(), "%d" % partition_id)
+            add_line("%d_LABEL" % partition_id, "%s" % name.upper())
+            partition_id += 1
+
         if dest is DEST_HEADER:
-            add_line("%s_DEV_NAME" % name.upper(), "\"NRF_FLASH_DRV_NAME\"")
+            if partition_has_device(partition):
+                add_line("%s_DEV_NAME" % name.upper(), f"\"{regions_config[partition['region']]['device']}\"")
         elif dest is DEST_KCONFIG:
             if 'span' in partition.keys():
                 add_line("%s_SPAN" % name.upper(), string_of_strings(partition['span']))
 
-        pm_config[name]['partition_id'] = partition_id
-        partition_id += 1
     add_line("NUM", "%d" % partition_id)
 
-    def find_depth(key, depth = 0):
+    def find_depth(key, depth=0):
         if 'span' in pm_config[key].keys():
             return find_depth(pm_config[key]['span'][0], depth + 1)
         return depth
 
-    all_by_size = list(pm_config.keys())
-
+    flash_partition_pm_config = {k: v for k, v in pm_config.items() if partition_has_device(v)}
+    all_by_size = list(flash_partition_pm_config.keys())
     all_by_size = sorted(all_by_size, key=find_depth)
-    all_by_size = sorted(all_by_size, key=lambda key: pm_config[key]['size'])
+    all_by_size = sorted(all_by_size, key=lambda key: flash_partition_pm_config[key]['size'])
     add_line("ALL_BY_SIZE", string_of_strings(all_by_size))
 
     return config_lines
@@ -69,9 +78,9 @@ def write_config_lines_to_file(pm_config_file_path, config_lines):
         out_file.write('\n'.join(config_lines))
 
 
-def write_pm_config(pm_config, name, out_path):
+def write_pm_config(pm_config, regions_config, name, out_path):
     pm_config_file = path.basename(out_path)
-    config_lines = get_config_lines(pm_config, "#define ", " ", DEST_HEADER)
+    config_lines = get_config_lines(pm_config, regions_config, "#define ", " ", DEST_HEADER)
 
     image_config_lines = list.copy(config_lines)
     image_config_lines.append("#define PM_ADDRESS 0x%x" % pm_config[name]['address'])
@@ -81,8 +90,8 @@ def write_pm_config(pm_config, name, out_path):
     write_config_lines_to_file(out_path, image_config_lines)
 
 
-def write_kconfig_file(pm_config, out_path):
-    config_lines = get_config_lines(pm_config, "", "=", DEST_KCONFIG)
+def write_kconfig_file(pm_config, regions_config, out_path):
+    config_lines = get_config_lines(pm_config, regions_config, "", "=", DEST_KCONFIG)
     write_config_lines_to_file(out_path, config_lines)
 
 
@@ -91,8 +100,11 @@ def parse_args():
         description='''Creates files based on Partition Manager results.''',
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument("--input", required=True, type=str,
-                        help="Path to the input .yml file.")
+    parser.add_argument("--input-partitions", required=True, type=str,
+                        help="Path to the input .yml file with partition configuration.")
+
+    parser.add_argument("--input-regions", required=True, type=str,
+                        help="Path to the input .yml file with region configuration.")
 
     parser.add_argument("--config-file", required=True, type=str,
                         help="Path to the output .config file.")
@@ -110,13 +122,15 @@ def parse_args():
 def main():
     args = parse_args()
 
-    with open(args.input, 'r') as f:
+    with open(args.input_partitions, 'r') as f:
         pm_config = yaml.safe_load(f)
+    with open(args.input_regions, 'r') as f:
+        regions_config = yaml.safe_load(f)
 
-    write_kconfig_file(pm_config, args.config_file)
+    write_kconfig_file(pm_config, regions_config, args.config_file)
 
     for name, header_file in zip(args.input_names, args.header_files):
-        write_pm_config(pm_config, name, header_file)
+        write_pm_config(pm_config, regions_config, name, header_file)
 
 
 if __name__ == "__main__":