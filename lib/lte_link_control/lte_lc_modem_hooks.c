eivindj-nordic/sdk-nrf

lit: lte_lc: do not log error when DFU update is successful
Do not log error in LTE LC modem init hook when DFU update is
successful, as this is expected.

Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

lib/lte_link_control/lte_lc_modem_hooks.c
@@ -17,6 +17,10 @@ NRF_MODEM_LIB_ON_SHUTDOWN(lte_lc_shutdown_hook, on_modem_shutdown, NULL);
 static void on_modem_init(int err, void *ctx)
 {
 	if (err) {
+		if (err == NRF_MODEM_DFU_RESULT_OK) {
+			LOG_INF("Modem DFU, lte_lc not initialized");
+			return;
+		}
 		LOG_ERR("Modem library init error: %d, lte_lc not initialized", err);
 		return;
 	}

Sandy6Zhou/nrf

lib/lte_link_control/lte_lc_modem_hooks.c
@@ -17,6 +17,10 @@ NRF_MODEM_LIB_ON_SHUTDOWN(lte_lc_shutdown_hook, on_modem_shutdown, NULL);
 static void on_modem_init(int err, void *ctx)
 {
 	if (err) {
+		if (err == NRF_MODEM_DFU_RESULT_OK) {
+			LOG_DBG("Modem DFU, lte_lc not initialized");
+			return;
+		}
 		LOG_ERR("Modem library init error: %d, lte_lc not initialized", err);
 		return;
 	}