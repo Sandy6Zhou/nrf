oyvindronningstad/sdk-nrf

subsys: bootloader: bl_crypto: Prepare and expose functions as extabis
 - Add a crypto_init() for explicit initialization.
   This is in preparation for exposing the API to the app.
 - Add "external" argument to functions to signify that the functions
   should not use static memory.
 - Add crypto_root_of_trust as extabi
 - Add ecdsa and sha256 functions as extabi
 - Add bl_crypto_client.c as backend when using the ABI from the app.
 - The bl_crypto test now uses the ABI instead of linking with the
   library.
 - Add more test cases and a test generator which generates a
   header file to be used in the test. One case contains a real firmware
   image, hash, public key and signature. Which is used by the MCUBoot
   test case.

Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>
Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

subsys/bootloader/bl_crypto/bl_crypto.c
@@ -7,22 +7,140 @@
 #include "debug.h"
 #include <zephyr/types.h>
 #include <bl_crypto.h>
+#include <fw_metadata.h>
+#include <assert.h>
+#include <occ_constant_time.h>
 #include "bl_crypto_internal.h"
 
-int crypto_root_of_trust(const u8_t *public_key, const u8_t *public_key_hash,
-			 const u8_t *signature, const u8_t *firmware,
-			 const u32_t firmware_len)
-{
-	__ASSERT(public_key && public_key_hash && signature && firmware,
-			"A parameter was NULL.");
 
-	if (!verify_truncated_hash(public_key, CONFIG_SB_PUBLIC_KEY_LEN,
-			public_key_hash, CONFIG_SB_PUBLIC_KEY_HASH_LEN)) {
-		return -EPKHASHINV;
+__weak int crypto_init_signing(void)
+{
+	return 0;
+}
+
+__weak int crypto_init_hash(void)
+{
+	return 0;
+}
+
+int crypto_init(void)
+{
+	int retval = crypto_init_signing();
+	if (retval) {
+		return retval;
 	}
 
-	if (!verify_signature(firmware, firmware_len, signature, public_key)) {
-		return -ESIGINV;
+	return crypto_init_hash();
+}
+
+BUILD_ASSERT_MSG(CONFIG_SB_PUBLIC_KEY_HASH_LEN <= CONFIG_SB_HASH_LEN,
+		"Invalid value for SB_PUBLIC_KEY_HASH_LEN.");
+
+static int verify_truncated_hash(const u8_t *data, u32_t data_len,
+		const u8_t *expected, u32_t hash_len, bool external)
+{
+	u8_t hash[CONFIG_SB_HASH_LEN];
+
+	__ASSERT(hash_len <= CONFIG_SB_HASH_LEN, "truncated hash length too long.");
+
+	int retval = get_hash(hash, data, data_len, external);
+	if (retval != 0) {
+		return retval;
+	}
+	if (!occ_constant_time_equal(expected, hash, hash_len)) {
+		return -EHASHINV;
 	}
 	return 0;
 }
+
+static int verify_signature(const u8_t *data, u32_t data_len,
+		const u8_t *signature, const u8_t *public_key, bool external)
+{
+	u8_t hash1[CONFIG_SB_HASH_LEN];
+	u8_t hash2[CONFIG_SB_HASH_LEN];
+
+	int retval = get_hash(hash1, data, data_len, external);
+	if (retval != 0) {
+		return retval;
+	}
+
+	retval = get_hash(hash2, hash1, CONFIG_SB_HASH_LEN, external);
+	if (retval != 0) {
+		return retval;
+	}
+
+	return bl_secp256r1_validate(hash2, CONFIG_SB_HASH_LEN, public_key, signature);
+}
+
+/* Base implementation, with 'external' parameter. */
+static int _root_of_trust_verify(
+		const u8_t *public_key, const u8_t *public_key_hash,
+		const u8_t *signature, const u8_t *firmware,
+		const u32_t firmware_len, bool external)
+{
+	__ASSERT(public_key && public_key_hash && signature && firmware,
+			"A parameter was NULL.");
+	int retval = verify_truncated_hash(public_key, CONFIG_SB_PUBLIC_KEY_LEN,
+			public_key_hash, CONFIG_SB_PUBLIC_KEY_HASH_LEN, external);
+
+	if (retval != 0) {
+		return retval;
+	}
+
+	return verify_signature(firmware, firmware_len, signature, public_key,
+			external);
+}
+
+
+/* For use by the bootloader. */
+int root_of_trust_verify(const u8_t *public_key, const u8_t *public_key_hash,
+			 const u8_t *signature, const u8_t *firmware,
+			 const u32_t firmware_len)
+{
+	return _root_of_trust_verify(public_key, public_key_hash, signature,
+					firmware, firmware_len, false);
+}
+
+
+/* For use through ext_abi. */
+int root_of_trust_verify_external(
+			const u8_t *public_key, const u8_t *public_key_hash,
+			const u8_t *signature, const u8_t *firmware,
+			const u32_t firmware_len)
+{
+	return _root_of_trust_verify(public_key, public_key_hash, signature,
+					firmware, firmware_len, true);
+}
+
+int bl_sha256_verify(const u8_t *data, u32_t data_len, const u8_t *expected)
+{
+	return verify_truncated_hash(data, data_len, expected, CONFIG_SB_HASH_LEN, true);
+}
+
+__ext_abi(struct rot_verify_abi, rot_verify_abi) = {
+	.header = ABI_INFO_INIT(ROT_VERIFY_ABI_ID, ROT_VERIFY_ABI_FLAGS,
+				ROT_VERIFY_ABI_VER, sizeof(struct rot_verify_abi)),
+	.abi = {
+		.root_of_trust_verify = root_of_trust_verify_external,
+	}
+};
+
+__ext_abi(struct bl_sha256_abi, bl_sha256_abi) = {
+	.header = ABI_INFO_INIT(BL_SHA256_ABI_ID, BL_SHA256_ABI_FLAGS,
+				BL_SHA256_ABI_VER, sizeof(struct bl_sha256_abi)),
+	.abi = {
+		.bl_sha256_init = bl_sha256_init,
+		.bl_sha256_update = bl_sha256_update,
+		.bl_sha256_finalize = bl_sha256_finalize,
+		.bl_sha256_verify = bl_sha256_verify,
+		.bl_sha256_ctx_size = SHA256_CTX_SIZE,
+	},
+};
+
+__ext_abi(struct bl_secp256r1_abi, bl_secp256r1_abi) = {
+	.header = ABI_INFO_INIT(BL_SECP256R1_ABI_ID, BL_SECP256R1_ABI_FLAGS,
+				BL_SECP256R1_ABI_VER, sizeof(struct bl_secp256r1_abi)),
+	.abi = {
+		.bl_secp256r1_validate = bl_secp256r1_validate,
+	},
+};

tests/subsys/bootloader/bl_crypto/src/main.c
@@ -10,25 +10,196 @@
 #include "test_vector.c"
 
 
-void test_crypto_root_of_trust(void)
+void test_ecdsa_verify(void)
 {
+	u32_t hash_len = ARRAY_SIZE(hash_sha256);
+
+	/* Success */
+	int retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_hash_sha256, hash_len, pub_concat, sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, const_pub_concat, sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, const_sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, const_pub_concat, const_sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_hash_sha256, hash_len, const_pub_concat, const_sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_hash_sha256, hash_len, const_pub_concat, const_sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, image_gen_sig);
+	zassert_equal(0, retval, "gen sig failed retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, image_fw_sig);
+	zassert_equal(0, retval, "fw_sig retval: 0x%x", retval);
+	image_fw_hash[0]++;
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, image_fw_sig);
+	image_fw_hash[0]--;
+	zassert_not_equal(0, retval, "fw_sig retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_fw_hash, hash_len, image_public_key, image_fw_sig);
+	zassert_equal(0, retval, "const_fw retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, const_public_key, image_fw_sig);
+	zassert_equal(0, retval, "const_pubk retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, const_gen_sig);
+	zassert_equal(0, retval, "const_gen_sig retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, const_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig const_pk retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_fw_hash, hash_len, const_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig const_pk const_fw retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_fw_hash, hash_len, image_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig const_fw retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_fw_hash, hash_len, const_public_key, image_fw_sig);
+	zassert_equal(0, retval, "const_pk const_fw retval: 0x%x", retval);
+
+	/* pub key does not match */
+	pub_concat[1]++;
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	pub_concat[1]--;
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+
+	pub_concat[1]++;
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, const_sig_concat);
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+	retval = bl_secp256r1_validate(const_hash_sha256, hash_len, pub_concat, const_sig_concat);
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+	pub_concat[1]--;
+
+	/* hash does not match */
+	hash_sha256[2]++;
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	hash_sha256[2]--;
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+
+	/* signature does not match */
+	sig_concat[3]++;
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	sig_concat[3]--;
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+
+	/* Null pointer passed as hash */
+	retval = bl_secp256r1_validate(NULL, hash_len, pub_concat, sig_concat);
+	zassert_equal(-EINVAL, retval, "retval: 0x%x", retval);
+
+	/* Wrong hash LEN */
+	retval = bl_secp256r1_validate(hash_sha256, 24, pub_concat, sig_concat);
+	zassert_equal(-EINVAL, retval, "retval: 0x%x", retval);
+
+	/* Null pointer passed as public key */
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, NULL, sig_concat);
+	zassert_equal(-EINVAL, retval, "retval: 0x%x", retval);
+
+	/* Null pointer passed as signature */
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, NULL);
+	zassert_equal(-EINVAL, retval, "retval: 0x%x", retval);
+}
+
+void test_sha256_string(const uint8_t * input, uint32_t input_len, const uint8_t * test_vector, bool eq)
+{
+	int rc = 0;
+	uint8_t output[32] = {0};
+	bl_sha256_ctx_t ctx;
+	static u32_t run_count = 0;
+
+	rc = bl_sha256_init(&ctx);
+	rc = bl_sha256_update(&ctx, input, input_len);
+	rc = bl_sha256_finalize(&ctx, output);
+	zassert_equal(0, rc, "hash updated failed retval was: %d");
+
+	for(size_t i = 0; i < ARRAY_SIZE(output); i++) {
+		if(eq){
+			zassert_equal(test_vector[i], output[i], "positive test failed (run no. %d) for i = %d", run_count, i);
+		}else {
+			zassert_not_equal(test_vector[i], output[i], "negative test failed (run no. %d) for i = %d", run_count, i);
+		}
+	}
+
+	rc = bl_sha256_verify(input, input_len, test_vector);
+
+	int expected_rc = eq ? 0 : -EHASHINV;
+	zassert_equal(expected_rc, rc, "bl_sha256_verify returned %d instead of %d", rc, expected_rc);
+
+	run_count++;
+}
+const uint8_t input3[] = "test vector should fail";
+const uint8_t input2[] = "test vector";
+
+void test_sha256(void)
+{
+	test_sha256_string(NULL, 0, sha256_empty_string, true);
+	test_sha256_string(const_fw_data, ARRAY_SIZE(const_fw_data), image_fw_hash, true);
+	test_sha256_string(input2, strlen(input2), sha256_test_vector_string, true);
+
+	test_sha256_string(input3, strlen(input3), sha256_test_vector_string, false);
+	test_sha256_string(mcuboot_key, ARRAY_SIZE(mcuboot_key), mcuboot_key_hash, true);
+	test_sha256_string(mcuboot_key, ARRAY_SIZE(mcuboot_key), sha256_test_vector_string, false);
+
+	/* Size restrictions. */
+#if CONFIG_FLASH_SIZE > 300
+	test_sha256_string(long_input, ARRAY_SIZE(long_input), long_input_hash, true);
+	test_sha256_string(long_input, ARRAY_SIZE(long_input), sha256_test_vector_string, false);
+#endif
+
+	test_sha256_string(hash_in1, 1, hash_res1, true);
+	test_sha256_string(hash_in2, 3, hash_res2, true);
+	test_sha256_string(hash_in3, 56, hash_res3, true);
+
+	test_sha256_string(hash_in, 55, hash_res55, true);
+	test_sha256_string(hash_in, 56, hash_res56, true);
+	test_sha256_string(hash_in, 57, hash_res57, true);
+	test_sha256_string(hash_in, 63, hash_res63, true);
+	test_sha256_string(hash_in, 64, hash_res64, true);
+	test_sha256_string(hash_in, 65, hash_res65, true);
+}
+
+void test_root_of_trust_verify(void)
+{
+
 	/* Success. */
-	int retval = crypto_root_of_trust(pk, pk_hash, sig, firmware,
+	int retval = root_of_trust_verify(pk, pk_hash, sig, firmware,
 			sizeof(firmware));
 
 	zassert_equal(0, retval, "retval was %d", retval);
 
+	retval = root_of_trust_verify(const_pk, const_pk_hash, const_sig, const_firmware,
+			sizeof(const_firmware));
+	zassert_equal(0, retval, "retval was %d", retval);
+
+	zassert_equal(0, retval, "retval was %d", retval);
 	/* pk doesn't match pk_hash. */
 	pk[1]++;
-	retval = crypto_root_of_trust(pk, pk_hash, sig, firmware,
+	retval = root_of_trust_verify(pk, pk_hash, sig, firmware,
 			sizeof(firmware));
 	pk[1]--;
 
-	zassert_equal(-EPKHASHINV, retval, "retval was %d", retval);
+	zassert_equal(-EHASHINV, retval, "retval was %d", retval);
 
 	/* metadata doesn't match signature */
 	firmware[0]++;
-	retval = crypto_root_of_trust(pk, pk_hash, sig, firmware,
+	retval = root_of_trust_verify(pk, pk_hash, sig, firmware,
 			sizeof(firmware));
 	firmware[0]--;
 
@@ -38,6 +209,9 @@ void test_crypto_root_of_trust(void)
 void test_main(void)
 {
 	ztest_test_suite(test_bl_crypto,
-			 ztest_unit_test(test_crypto_root_of_trust));
+			 ztest_unit_test(test_root_of_trust_verify),
+			 ztest_unit_test(test_sha256),
+			 ztest_unit_test(test_ecdsa_verify)
+	);
 	ztest_run_test_suite(test_bl_crypto);
 }

Sandy6Zhou/nrf

subsys/bootloader/bl_crypto/bl_crypto.c
@@ -7,22 +7,140 @@
 #include "debug.h"
 #include <zephyr/types.h>
 #include <bl_crypto.h>
+#include <fw_metadata.h>
+#include <assert.h>
+#include <occ_constant_time.h>
 #include "bl_crypto_internal.h"
 
-int crypto_root_of_trust(const u8_t *public_key, const u8_t *public_key_hash,
-			 const u8_t *signature, const u8_t *firmware,
-			 const u32_t firmware_len)
-{
-	__ASSERT(public_key && public_key_hash && signature && firmware,
-			"A parameter was NULL.");
 
-	if (!verify_truncated_hash(public_key, CONFIG_SB_PUBLIC_KEY_LEN,
-			public_key_hash, CONFIG_SB_PUBLIC_KEY_HASH_LEN)) {
-		return -EPKHASHINV;
+__weak int crypto_init_signing(void)
+{
+	return 0;
+}
+
+__weak int crypto_init_hash(void)
+{
+	return 0;
+}
+
+int bl_crypto_init(void)
+{
+	int retval = crypto_init_signing();
+	if (retval) {
+		return retval;
 	}
 
-	if (!verify_signature(firmware, firmware_len, signature, public_key)) {
-		return -ESIGINV;
+	return crypto_init_hash();
+}
+
+BUILD_ASSERT_MSG(CONFIG_SB_PUBLIC_KEY_HASH_LEN <= CONFIG_SB_HASH_LEN,
+		"Invalid value for SB_PUBLIC_KEY_HASH_LEN.");
+
+static int verify_truncated_hash(const u8_t *data, u32_t data_len,
+		const u8_t *expected, u32_t hash_len, bool external)
+{
+	u8_t hash[CONFIG_SB_HASH_LEN];
+
+	__ASSERT(hash_len <= CONFIG_SB_HASH_LEN, "truncated hash length too long.");
+
+	int retval = get_hash(hash, data, data_len, external);
+	if (retval != 0) {
+		return retval;
+	}
+	if (!occ_constant_time_equal(expected, hash, hash_len)) {
+		return -EHASHINV;
 	}
 	return 0;
 }
+
+static int verify_signature(const u8_t *data, u32_t data_len,
+		const u8_t *signature, const u8_t *public_key, bool external)
+{
+	u8_t hash1[CONFIG_SB_HASH_LEN];
+	u8_t hash2[CONFIG_SB_HASH_LEN];
+
+	int retval = get_hash(hash1, data, data_len, external);
+	if (retval != 0) {
+		return retval;
+	}
+
+	retval = get_hash(hash2, hash1, CONFIG_SB_HASH_LEN, external);
+	if (retval != 0) {
+		return retval;
+	}
+
+	return bl_secp256r1_validate(hash2, CONFIG_SB_HASH_LEN, public_key, signature);
+}
+
+/* Base implementation, with 'external' parameter. */
+static int root_of_trust_verify(
+		const u8_t *public_key, const u8_t *public_key_hash,
+		const u8_t *signature, const u8_t *firmware,
+		const u32_t firmware_len, bool external)
+{
+	__ASSERT(public_key && public_key_hash && signature && firmware,
+			"A parameter was NULL.");
+	int retval = verify_truncated_hash(public_key, CONFIG_SB_PUBLIC_KEY_LEN,
+			public_key_hash, CONFIG_SB_PUBLIC_KEY_HASH_LEN, external);
+
+	if (retval != 0) {
+		return retval;
+	}
+
+	return verify_signature(firmware, firmware_len, signature, public_key,
+			external);
+}
+
+
+/* For use by the bootloader. */
+int bl_root_of_trust_verify(const u8_t *public_key, const u8_t *public_key_hash,
+			 const u8_t *signature, const u8_t *firmware,
+			 const u32_t firmware_len)
+{
+	return root_of_trust_verify(public_key, public_key_hash, signature,
+					firmware, firmware_len, false);
+}
+
+
+/* For use through ext_abi. */
+int bl_root_of_trust_verify_external(
+			const u8_t *public_key, const u8_t *public_key_hash,
+			const u8_t *signature, const u8_t *firmware,
+			const u32_t firmware_len)
+{
+	return root_of_trust_verify(public_key, public_key_hash, signature,
+					firmware, firmware_len, true);
+}
+
+int bl_sha256_verify(const u8_t *data, u32_t data_len, const u8_t *expected)
+{
+	return verify_truncated_hash(data, data_len, expected, CONFIG_SB_HASH_LEN, true);
+}
+
+__ext_abi(struct bl_rot_verify_abi, bl_rot_verify_abi) = {
+	.header = ABI_INFO_INIT(BL_ROT_VERIFY_ABI_ID, BL_ROT_VERIFY_ABI_FLAGS,
+				BL_ROT_VERIFY_ABI_VER, sizeof(struct bl_rot_verify_abi)),
+	.abi = {
+		.bl_root_of_trust_verify = bl_root_of_trust_verify_external,
+	}
+};
+
+__ext_abi(struct bl_sha256_abi, bl_sha256_abi) = {
+	.header = ABI_INFO_INIT(BL_SHA256_ABI_ID, BL_SHA256_ABI_FLAGS,
+				BL_SHA256_ABI_VER, sizeof(struct bl_sha256_abi)),
+	.abi = {
+		.bl_sha256_init = bl_sha256_init,
+		.bl_sha256_update = bl_sha256_update,
+		.bl_sha256_finalize = bl_sha256_finalize,
+		.bl_sha256_verify = bl_sha256_verify,
+		.bl_sha256_ctx_size = SHA256_CTX_SIZE,
+	},
+};
+
+__ext_abi(struct bl_secp256r1_abi, bl_secp256r1_abi) = {
+	.header = ABI_INFO_INIT(BL_SECP256R1_ABI_ID, BL_SECP256R1_ABI_FLAGS,
+				BL_SECP256R1_ABI_VER, sizeof(struct bl_secp256r1_abi)),
+	.abi = {
+		.bl_secp256r1_validate = bl_secp256r1_validate,
+	},
+};

tests/subsys/bootloader/bl_crypto/src/main.c
@@ -10,25 +10,196 @@
 #include "test_vector.c"
 
 
-void test_crypto_root_of_trust(void)
+void test_ecdsa_verify(void)
 {
+	u32_t hash_len = ARRAY_SIZE(hash_sha256);
+
+	/* Success */
+	int retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_hash_sha256, hash_len, pub_concat, sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, const_pub_concat, sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, const_sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, const_pub_concat, const_sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_hash_sha256, hash_len, const_pub_concat, const_sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_hash_sha256, hash_len, const_pub_concat, const_sig_concat);
+	zassert_equal(0, retval, "retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, image_gen_sig);
+	zassert_equal(0, retval, "gen sig failed retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, image_fw_sig);
+	zassert_equal(0, retval, "fw_sig retval: 0x%x", retval);
+	image_fw_hash[0]++;
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, image_fw_sig);
+	image_fw_hash[0]--;
+	zassert_not_equal(0, retval, "fw_sig retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_fw_hash, hash_len, image_public_key, image_fw_sig);
+	zassert_equal(0, retval, "const_fw retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, const_public_key, image_fw_sig);
+	zassert_equal(0, retval, "const_pubk retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, image_public_key, const_gen_sig);
+	zassert_equal(0, retval, "const_gen_sig retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(image_fw_hash, hash_len, const_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig const_pk retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_fw_hash, hash_len, const_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig const_pk const_fw retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_fw_hash, hash_len, image_public_key, const_fw_sig);
+	zassert_equal(0, retval, "const_sig const_fw retval: 0x%x", retval);
+
+	retval = bl_secp256r1_validate(const_fw_hash, hash_len, const_public_key, image_fw_sig);
+	zassert_equal(0, retval, "const_pk const_fw retval: 0x%x", retval);
+
+	/* pub key does not match */
+	pub_concat[1]++;
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	pub_concat[1]--;
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+
+	pub_concat[1]++;
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, const_sig_concat);
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+	retval = bl_secp256r1_validate(const_hash_sha256, hash_len, pub_concat, const_sig_concat);
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+	pub_concat[1]--;
+
+	/* hash does not match */
+	hash_sha256[2]++;
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	hash_sha256[2]--;
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+
+	/* signature does not match */
+	sig_concat[3]++;
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, sig_concat);
+	sig_concat[3]--;
+	zassert_equal(-ESIGINV, retval, "retval: 0x%x", retval);
+
+	/* Null pointer passed as hash */
+	retval = bl_secp256r1_validate(NULL, hash_len, pub_concat, sig_concat);
+	zassert_equal(-EINVAL, retval, "retval: 0x%x", retval);
+
+	/* Wrong hash LEN */
+	retval = bl_secp256r1_validate(hash_sha256, 24, pub_concat, sig_concat);
+	zassert_equal(-EINVAL, retval, "retval: 0x%x", retval);
+
+	/* Null pointer passed as public key */
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, NULL, sig_concat);
+	zassert_equal(-EINVAL, retval, "retval: 0x%x", retval);
+
+	/* Null pointer passed as signature */
+	retval = bl_secp256r1_validate(hash_sha256, hash_len, pub_concat, NULL);
+	zassert_equal(-EINVAL, retval, "retval: 0x%x", retval);
+}
+
+void test_sha256_string(const uint8_t * input, uint32_t input_len, const uint8_t * test_vector, bool eq)
+{
+	int rc = 0;
+	uint8_t output[32] = {0};
+	bl_sha256_ctx_t ctx;
+	static u32_t run_count = 0;
+
+	rc = bl_sha256_init(&ctx);
+	rc = bl_sha256_update(&ctx, input, input_len);
+	rc = bl_sha256_finalize(&ctx, output);
+	zassert_equal(0, rc, "hash updated failed retval was: %d");
+
+	for(size_t i = 0; i < ARRAY_SIZE(output); i++) {
+		if(eq){
+			zassert_equal(test_vector[i], output[i], "positive test failed (run no. %d) for i = %d", run_count, i);
+		}else {
+			zassert_not_equal(test_vector[i], output[i], "negative test failed (run no. %d) for i = %d", run_count, i);
+		}
+	}
+
+	rc = bl_sha256_verify(input, input_len, test_vector);
+
+	int expected_rc = eq ? 0 : -EHASHINV;
+	zassert_equal(expected_rc, rc, "bl_sha256_verify returned %d instead of %d", rc, expected_rc);
+
+	run_count++;
+}
+const uint8_t input3[] = "test vector should fail";
+const uint8_t input2[] = "test vector";
+
+void test_sha256(void)
+{
+	test_sha256_string(NULL, 0, sha256_empty_string, true);
+	test_sha256_string(const_fw_data, ARRAY_SIZE(const_fw_data), image_fw_hash, true);
+	test_sha256_string(input2, strlen(input2), sha256_test_vector_string, true);
+
+	test_sha256_string(input3, strlen(input3), sha256_test_vector_string, false);
+	test_sha256_string(mcuboot_key, ARRAY_SIZE(mcuboot_key), mcuboot_key_hash, true);
+	test_sha256_string(mcuboot_key, ARRAY_SIZE(mcuboot_key), sha256_test_vector_string, false);
+
+	/* Size restrictions. */
+#if CONFIG_FLASH_SIZE > 300
+	test_sha256_string(long_input, ARRAY_SIZE(long_input), long_input_hash, true);
+	test_sha256_string(long_input, ARRAY_SIZE(long_input), sha256_test_vector_string, false);
+#endif
+
+	test_sha256_string(hash_in1, 1, hash_res1, true);
+	test_sha256_string(hash_in2, 3, hash_res2, true);
+	test_sha256_string(hash_in3, 56, hash_res3, true);
+
+	test_sha256_string(hash_in, 55, hash_res55, true);
+	test_sha256_string(hash_in, 56, hash_res56, true);
+	test_sha256_string(hash_in, 57, hash_res57, true);
+	test_sha256_string(hash_in, 63, hash_res63, true);
+	test_sha256_string(hash_in, 64, hash_res64, true);
+	test_sha256_string(hash_in, 65, hash_res65, true);
+}
+
+void test_bl_root_of_trust_verify(void)
+{
+
 	/* Success. */
-	int retval = crypto_root_of_trust(pk, pk_hash, sig, firmware,
+	int retval = bl_root_of_trust_verify(pk, pk_hash, sig, firmware,
 			sizeof(firmware));
 
 	zassert_equal(0, retval, "retval was %d", retval);
 
+	retval = bl_root_of_trust_verify(const_pk, const_pk_hash, const_sig, const_firmware,
+			sizeof(const_firmware));
+	zassert_equal(0, retval, "retval was %d", retval);
+
+	zassert_equal(0, retval, "retval was %d", retval);
 	/* pk doesn't match pk_hash. */
 	pk[1]++;
-	retval = crypto_root_of_trust(pk, pk_hash, sig, firmware,
+	retval = bl_root_of_trust_verify(pk, pk_hash, sig, firmware,
 			sizeof(firmware));
 	pk[1]--;
 
-	zassert_equal(-EPKHASHINV, retval, "retval was %d", retval);
+	zassert_equal(-EHASHINV, retval, "retval was %d", retval);
 
 	/* metadata doesn't match signature */
 	firmware[0]++;
-	retval = crypto_root_of_trust(pk, pk_hash, sig, firmware,
+	retval = bl_root_of_trust_verify(pk, pk_hash, sig, firmware,
 			sizeof(firmware));
 	firmware[0]--;
 
@@ -38,6 +209,9 @@ void test_crypto_root_of_trust(void)
 void test_main(void)
 {
 	ztest_test_suite(test_bl_crypto,
-			 ztest_unit_test(test_crypto_root_of_trust));
+			 ztest_unit_test(test_bl_root_of_trust_verify),
+			 ztest_unit_test(test_sha256),
+			 ztest_unit_test(test_ecdsa_verify)
+	);
 	ztest_run_test_suite(test_bl_crypto);
 }