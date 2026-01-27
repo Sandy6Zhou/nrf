nordicjm/sdk-nrf

sysbuild: disable partition manager / child image when sysbuild is used
Sysbuild and parent / child image are mutual exclusive.

Thus it is not possible to use both at the same time.
In parent / child image, the partition manager is invoked in the parent
image, and also in each domain image.

With sysbuild, the partition manager is invoked in sysbuild.
Therefore when building images through sysbuild, then partition manager
must be disabled in the image build.

Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

cmake/multi_image.cmake
@@ -4,6 +4,15 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
+if(SYSBUILD)
+  # Sysbuild and child-image are mutual exclusive, so if sysbuild is used disable child-image
+  function(add_child_image)
+    set(CONFIG_USE_PARTITON_MANAGER n CACHE INTERNAL "")
+    # ignore, sysbuild is in use.
+  endfunction()
+  return()
+endif()
+
 if(IMAGE_NAME)
   set_shared(IMAGE ${IMAGE_NAME} PROPERTY KERNEL_HEX_NAME ${KERNEL_HEX_NAME})
   set_shared(IMAGE ${IMAGE_NAME} PROPERTY ZEPHYR_BINARY_DIR ${ZEPHYR_BINARY_DIR})

Sandy6Zhou/nrf

cmake/multi_image.cmake
@@ -4,6 +4,15 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
+if(SYSBUILD)
+  # Sysbuild and child-image are mutual exclusive, so if sysbuild is used disable child-image
+  function(add_child_image)
+    set(CONFIG_USE_PARTITION_MANAGER n CACHE INTERNAL "")
+    # ignore, sysbuild is in use.
+  endfunction()
+  return()
+endif()
+
 if(IMAGE_NAME)
   set_shared(IMAGE ${IMAGE_NAME} PROPERTY KERNEL_HEX_NAME ${KERNEL_HEX_NAME})
   set_shared(IMAGE ${IMAGE_NAME} PROPERTY ZEPHYR_BINARY_DIR ${ZEPHYR_BINARY_DIR})