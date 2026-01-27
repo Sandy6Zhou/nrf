zephyrproject-rtos/zephyr

samples: Bluetooth: df: Enable TX of mutliple CTEs in a per adv event
Since periodic advertising chains are available there is
a possibility to send multiple PDUs including CTE in
a periodic advertising event.

This commit enables such functionality in direction finding
transmitter sample.

Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

samples/bluetooth/direction_finding_connectionless_tx/src/main.c
@@ -18,6 +18,8 @@
 
 /* Length of CTE in unit of 8[us] */
 #define CTE_LEN (0x14U)
+/* Number of CTE send in single periodic advertising train */
+#define PER_ADV_EVENT_CTE_COUNT 5
 
 static void adv_sent_cb(struct bt_le_ext_adv *adv,
 			struct bt_le_ext_adv_sent_info *info);
@@ -50,19 +52,18 @@ static struct bt_le_per_adv_param per_adv_param = {
 static uint8_t ant_patterns[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA };
 #endif /* CONFIG_BT_CTLR_DF_ANT_SWITCH_TX */
 
-struct bt_df_adv_cte_tx_param cte_params = {
-	.cte_len = CTE_LEN,
-	.cte_count = 1,
+struct bt_df_adv_cte_tx_param cte_params = { .cte_len = CTE_LEN,
+					     .cte_count = PER_ADV_EVENT_CTE_COUNT,
 #if defined(CONFIG_BT_CTLR_DF_ANT_SWITCH_TX)
-	.cte_type = BT_HCI_LE_AOD_CTE_2US,
-	.num_ant_ids = ARRAY_SIZE(ant_patterns),
-	.ant_ids = ant_patterns
+					     .cte_type = BT_HCI_LE_AOD_CTE_2US,
+					     .num_ant_ids = ARRAY_SIZE(ant_patterns),
+					     .ant_ids = ant_patterns
 #else
-	.cte_type = BT_HCI_LE_AOA_CTE,
-	.num_ant_ids = 0,
-	.ant_ids = NULL
+					     .cte_type = BT_HCI_LE_AOA_CTE,
+					     .num_ant_ids = 0,
+					     .ant_ids = NULL
 #endif /* CONFIG_BT_CTLR_DF_ANT_SWITCH_TX */
-	};
+};
 
 static void adv_sent_cb(struct bt_le_ext_adv *adv,
 			struct bt_le_ext_adv_sent_info *info)

Sandy6Zhou/nrf

It includes a partial fix of zephyrproject-rtos/zephyr#41407.
(cherry picked from commit 50a2cdd75adab154c9007d2744c453dd56fd696d)
Signed-off-by: Ryan Chu <ryan.chu@nordicsemi.no>

samples/bluetooth/direction_finding_connectionless_tx/src/main.c
@@ -11,13 +11,14 @@
 #include <sys/printk.h>
 
 #include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
 #include <bluetooth/direction.h>
 #include <sys/byteorder.h>
 #include <sys/util.h>
 
 /* Length of CTE in unit of 8[us] */
 #define CTE_LEN (0x14U)
+/* Number of CTE send in single periodic advertising train */
+#define PER_ADV_EVENT_CTE_COUNT 5
 
 static void adv_sent_cb(struct bt_le_ext_adv *adv,
 			struct bt_le_ext_adv_sent_info *info);
@@ -54,19 +55,18 @@ static uint8_t ant_patterns[] = {0x2, 0x0, 0x5, 0x6, 0x1, 0x4, 0xC, 0x9, 0xE,
 				 0xD, 0x8, 0xA};
 #endif /* CONFIG_BT_DF_CTE_TX_AOD */
 
-const struct bt_df_adv_cte_tx_param cte_params = {
-	.cte_len = CTE_LEN,
-	.cte_count = 1,
+struct bt_df_adv_cte_tx_param cte_params = { .cte_len = CTE_LEN,
+					     .cte_count = PER_ADV_EVENT_CTE_COUNT,
 #if defined(CONFIG_BT_DF_CTE_TX_AOD)
-	.cte_type = BT_DF_CTE_TYPE_AOD_2US,
-	.num_ant_ids = ARRAY_SIZE(ant_patterns),
-	.ant_ids = ant_patterns
+					     .cte_type = BT_DF_CTE_TYPE_AOD_2US,
+					     .num_ant_ids = ARRAY_SIZE(ant_patterns),
+					     .ant_ids = ant_patterns
 #else
-	.cte_type = BT_DF_CTE_TYPE_AOA,
-	.num_ant_ids = 0,
-	.ant_ids = NULL
+					     .cte_type = BT_DF_CTE_TYPE_AOA,
+					     .num_ant_ids = 0,
+					     .ant_ids = NULL
 #endif /* CONFIG_BT_DF_CTE_TX_AOD */
-	};
+};
 
 static void adv_sent_cb(struct bt_le_ext_adv *adv,
 			struct bt_le_ext_adv_sent_info *info)