nrfconnect/sdk-nrf

lib: lte_lc: Integrate PDN library
Copies the PDN functionality from the PDN library that is about
to be deprecated, and adds it as a module in the LTE link
controller library.

The functionality is the same, but with naming changes to types
and APIs to align with the rest of LTE link controller.
In addition, PDN events are now part of the LTE LC events, and
there is no longer need for a separate event handler.

Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

lib/lte_link_control/include/modules/pdn.h
@@ -1,0 +1,38 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef PDN_H__
+#define PDN_H__
+
+#include <modem/lte_lc.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Internal PDN module functions */
+
+int pdn_ctx_create(uint8_t *cid);
+int pdn_ctx_configure(uint8_t cid, const char *apn, enum lte_lc_pdn_family family,
+		      struct lte_lc_pdp_context_opts *opt);
+int pdn_ctx_auth_set(uint8_t cid, enum lte_lc_pdn_auth method,
+		     const char *user, const char *password);
+int pdn_ctx_destroy(uint8_t cid);
+int pdn_activate(uint8_t cid, int *esm, enum lte_lc_pdn_family *family);
+int pdn_deactivate(uint8_t cid);
+int pdn_id_get(uint8_t cid);
+int pdn_dynamic_info_get(uint8_t cid, struct lte_lc_pdn_dynamic_info *pdn_info);
+int pdn_default_apn_get(char *buf, size_t len);
+
+#if CONFIG_LTE_LC_PDN_ESM_STRERROR
+const char *pdn_esm_strerror(int reason);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PDN_H__ */

lib/lte_link_control/lte_lc.c
@@ -25,6 +25,7 @@
 #include "modules/mdmev.h"
 #include "modules/ncellmeas.h"
 #include "modules/periodicsearchconf.h"
+#include "modules/pdn.h"
 #include "modules/psm.h"
 #include "modules/xmodemsleep.h"
 #include "modules/xsystemmode.h"
@@ -224,6 +225,62 @@ int lte_lc_periodic_search_get(struct lte_lc_periodic_search_cfg *const cfg)
 {
 	return periodicsearchconf_get(cfg);
 }
+
+/* PDN API wrappers */
+
+int lte_lc_context_create(uint8_t *cid)
+{
+	return pdn_ctx_create(cid);
+}
+
+int lte_lc_context_configure(uint8_t cid, const char *apn, enum lte_lc_pdn_family family,
+			     struct lte_lc_pdp_context_opts *opts)
+{
+	return pdn_ctx_configure(cid, apn, family, opts);
+}
+
+int lte_lc_context_auth_set(uint8_t cid, enum lte_lc_pdn_auth method,
+			    const char *user, const char *password)
+{
+	return pdn_ctx_auth_set(cid, method, user, password);
+}
+
+int lte_lc_context_destroy(uint8_t cid)
+{
+	return pdn_ctx_destroy(cid);
+}
+
+int lte_lc_context_activate(uint8_t cid, int *esm, enum lte_lc_pdn_family *family)
+{
+	return pdn_activate(cid, esm, family);
+}
+
+int lte_lc_context_deactivate(uint8_t cid)
+{
+	return pdn_deactivate(cid);
+}
+
+int lte_lc_context_pdn_id_get(uint8_t cid)
+{
+	return pdn_id_get(cid);
+}
+
+int lte_lc_context_dynamic_info_get(uint8_t cid, struct lte_lc_pdn_dynamic_info *info)
+{
+	return pdn_dynamic_info_get(cid, info);
+}
+
+int lte_lc_default_apn_get(char *buf, size_t len)
+{
+	return pdn_default_apn_get(buf, len);
+}
+
+#if CONFIG_LTE_LC_PDN_ESM_STRERROR
+const char *lte_lc_pdn_esm_strerror(int reason)
+{
+	return pdn_esm_strerror(reason);
+}
+#endif
 
 static int lte_lc_sys_init(void)
 {

Sandy6Zhou/nrf

lib/lte_link_control/include/modules/pdn.h
@@ -1,0 +1,38 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef PDN_H__
+#define PDN_H__
+
+#include <modem/lte_lc.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Internal PDN module functions */
+
+int pdn_ctx_create(uint8_t *cid);
+int pdn_ctx_configure(uint8_t cid, const char *apn, enum lte_lc_pdn_family family,
+		      struct lte_lc_pdn_pdp_context_opts *opt);
+int pdn_ctx_auth_set(uint8_t cid, enum lte_lc_pdn_auth method,
+		     const char *user, const char *password);
+int pdn_ctx_destroy(uint8_t cid);
+int pdn_activate(uint8_t cid, int *esm, enum lte_lc_pdn_family *family);
+int pdn_deactivate(uint8_t cid);
+int pdn_id_get(uint8_t cid);
+int pdn_dynamic_info_get(uint8_t cid, struct lte_lc_pdn_dynamic_info *pdn_info);
+int pdn_ctx_default_apn_get(char *buf, size_t len);
+
+#if defined(CONFIG_LTE_LC_PDN_ESM_STRERROR) || defined(__DOXYGEN__)
+const char *pdn_esm_strerror(int reason);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PDN_H__ */

lib/lte_link_control/lte_lc.c
@@ -25,6 +25,7 @@
 #include "modules/mdmev.h"
 #include "modules/ncellmeas.h"
 #include "modules/periodicsearchconf.h"
+#include "modules/pdn.h"
 #include "modules/psm.h"
 #include "modules/xmodemsleep.h"
 #include "modules/xsystemmode.h"
@@ -224,6 +225,62 @@ int lte_lc_periodic_search_get(struct lte_lc_periodic_search_cfg *const cfg)
 {
 	return periodicsearchconf_get(cfg);
 }
+
+/* PDN API wrappers */
+
+int lte_lc_pdn_ctx_create(uint8_t *cid)
+{
+	return pdn_ctx_create(cid);
+}
+
+int lte_lc_pdn_ctx_configure(uint8_t cid, const char *apn, enum lte_lc_pdn_family family,
+			     struct lte_lc_pdn_pdp_context_opts *opts)
+{
+	return pdn_ctx_configure(cid, apn, family, opts);
+}
+
+int lte_lc_pdn_ctx_auth_set(uint8_t cid, enum lte_lc_pdn_auth method,
+			    const char *user, const char *password)
+{
+	return pdn_ctx_auth_set(cid, method, user, password);
+}
+
+int lte_lc_pdn_ctx_destroy(uint8_t cid)
+{
+	return pdn_ctx_destroy(cid);
+}
+
+int lte_lc_pdn_activate(uint8_t cid, int *esm, enum lte_lc_pdn_family *family)
+{
+	return pdn_activate(cid, esm, family);
+}
+
+int lte_lc_pdn_deactivate(uint8_t cid)
+{
+	return pdn_deactivate(cid);
+}
+
+int lte_lc_pdn_id_get(uint8_t cid)
+{
+	return pdn_id_get(cid);
+}
+
+int lte_lc_pdn_dynamic_info_get(uint8_t cid, struct lte_lc_pdn_dynamic_info *info)
+{
+	return pdn_dynamic_info_get(cid, info);
+}
+
+int lte_lc_pdn_ctx_default_apn_get(char *buf, size_t len)
+{
+	return pdn_ctx_default_apn_get(buf, len);
+}
+
+#if defined(CONFIG_LTE_LC_PDN_ESM_STRERROR)
+const char *lte_lc_pdn_esm_strerror(int reason)
+{
+	return pdn_esm_strerror(reason);
+}
+#endif
 
 static int lte_lc_sys_init(void)
 {