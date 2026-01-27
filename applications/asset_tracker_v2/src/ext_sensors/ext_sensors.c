maxd-nordic/sdk-nrf

asset_tracker v2: add impact detection
This patch adds impact detection using the ADXL372 accelerometer.

Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

applications/asset_tracker_v2/src/ext_sensors/ext_sensors.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <zephyr/drivers/sensor.h>
 #include <stdlib.h>
+#include <math.h>
 
 #if defined(CONFIG_EXTERNAL_SENSORS_BME680_BSEC)
 #include "ext_sensors_bsec.h"
@@ -57,9 +58,16 @@ static struct env_sensor press_sensor = {
 	.dev = DEVICE_DT_GET(DT_ALIAS(pressure_sensor)),
 };
 
-static struct env_sensor accel_sensor = {
+/** Sensor struct for the low-power accelerometer */
+static struct env_sensor accel_sensor_lp = {
 	.channel = SENSOR_CHAN_ACCEL_XYZ,
 	.dev = DEVICE_DT_GET(DT_ALIAS(accelerometer)),
+};
+
+/** Sensor struct for the high-G accelerometer */
+static struct env_sensor accel_sensor_hg = {
+	.channel = SENSOR_CHAN_ACCEL_XYZ,
+	.dev = DEVICE_DT_GET(DT_ALIAS(impact_sensor)),
 };
 
 static ext_sensor_handler_t evt_handler;
@@ -108,10 +116,46 @@ static void accelerometer_trigger_handler(const struct device *dev,
 		     (abs(evt.value_array[1]) > threshold) ||
 		     (abs(evt.value_array[2]) > threshold))) {
 
-			evt.type = EXT_SENSOR_EVT_ACCELEROMETER_TRIGGER;
+			evt.type = EXT_SENSOR_EVT_ACCELEROMETER_LP_TRIGGER;
 			evt_handler(&evt);
 		}
 
+		break;
+	default:
+		LOG_ERR("Unknown trigger");
+	}
+}
+
+static void impact_trigger_handler(const struct device *dev,
+				   const struct sensor_trigger *trig)
+{
+	struct sensor_value data[ACCELEROMETER_CHANNELS];
+	struct ext_sensor_evt evt = {0};
+	int err;
+
+	switch (trig->type) {
+	case SENSOR_TRIG_THRESHOLD:
+		if (sensor_sample_fetch(dev) < 0) {
+			LOG_ERR("Sample fetch error");
+			return;
+		}
+
+		err = sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, data);
+		if (err) {
+			LOG_ERR("sensor_channel_get, error: %d", err);
+			return;
+		}
+
+		evt.value = sqrt(pow(sensor_ms2_to_g(&data[0]), 2.0) +
+				 pow(sensor_ms2_to_g(&data[1]), 2.0) +
+				 pow(sensor_ms2_to_g(&data[2]), 2.0));
+
+		LOG_DBG("Detected impact of %6.2f g\n", evt.value);
+
+		if (evt.value > 0.0) {
+			evt.type = EXT_SENSOR_EVT_ACCELEROMETER_HG_TRIGGER;
+			evt_handler(&evt);
+		}
 		break;
 	default:
 		LOG_ERR("Unknown trigger");
@@ -157,26 +201,43 @@ int ext_sensors_init(ext_sensor_handler_t handler)
 	}
 #endif /* if defined(CONFIG_EXTERNAL_SENSORS_BME680_BSEC) */
 
-	if (!device_is_ready(accel_sensor.dev)) {
-		LOG_ERR("Accelerometer device is not ready");
+	if (!device_is_ready(accel_sensor_lp.dev)) {
+		LOG_ERR("Low-power accelerometer device is not ready");
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
-	} else {
-#if defined(CONFIG_EXTERNAL_SENSORS_ACTIVITY_DETECTION_AUTO)
+	} else if (IS_ENABLED(CONFIG_EXTERNAL_SENSORS_ACTIVITY_DETECTION_AUTO)) {
 		struct sensor_trigger trig = {
 			.chan = SENSOR_CHAN_ACCEL_XYZ,
 			.type = SENSOR_TRIG_THRESHOLD
 		};
 
-		int err = sensor_trigger_set(accel_sensor.dev,
+		int err = sensor_trigger_set(accel_sensor_lp.dev,
 					     &trig, accelerometer_trigger_handler);
 
 		if (err) {
 			LOG_ERR("Could not set trigger for device %s, error: %d",
-				accel_sensor.dev->name, err);
+				accel_sensor_lp.dev->name, err);
 			return err;
 		}
-#endif
+	}
+
+	if (!device_is_ready(accel_sensor_hg.dev)) {
+		LOG_ERR("High-G accelerometer device is not ready");
+		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
+		evt_handler(&evt);
+	} else if (IS_ENABLED(CONFIG_EXTERNAL_SENSORS_IMPACT_DETECTION_AUTO)) {
+		struct sensor_trigger trig = {
+			.chan = SENSOR_CHAN_ACCEL_XYZ,
+			.type = SENSOR_TRIG_THRESHOLD
+		};
+
+		int err = sensor_trigger_set(accel_sensor_hg.dev,
+					     &trig, impact_trigger_handler);
+		if (err) {
+			LOG_ERR("Could not set trigger for device %s, error: %d",
+				accel_sensor_hg.dev->name, err);
+			return err;
+		}
 	}
 
 	return 0;
@@ -329,34 +390,34 @@ int ext_sensors_mov_thres_set(double threshold_new)
 		.val1 = input_value
 	};
 
-	err = sensor_attr_set(accel_sensor.dev, SENSOR_CHAN_ACCEL_X,
+	err = sensor_attr_set(accel_sensor_lp.dev, SENSOR_CHAN_ACCEL_X,
 			      SENSOR_ATTR_UPPER_THRESH, &data);
 	if (err) {
 		LOG_ERR("Failed to set accelerometer x-axis threshold value");
 		LOG_ERR("Device: %s, error: %d",
-			accel_sensor.dev->name, err);
+			accel_sensor_lp.dev->name, err);
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
 		return err;
 	}
 
-	err = sensor_attr_set(accel_sensor.dev, SENSOR_CHAN_ACCEL_Y,
+	err = sensor_attr_set(accel_sensor_lp.dev, SENSOR_CHAN_ACCEL_Y,
 			      SENSOR_ATTR_UPPER_THRESH, &data);
 	if (err) {
 		LOG_ERR("Failed to set accelerometer y-axis threshold value");
 		LOG_ERR("Device: %s, error: %d",
-			accel_sensor.dev->name, err);
+			accel_sensor_lp.dev->name, err);
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
 		return err;
 	}
 
-	err = sensor_attr_set(accel_sensor.dev, SENSOR_CHAN_ACCEL_Z,
+	err = sensor_attr_set(accel_sensor_lp.dev, SENSOR_CHAN_ACCEL_Z,
 			      SENSOR_ATTR_UPPER_THRESH, &data);
 	if (err) {
 		LOG_ERR("Failed to set accelerometer z-axis threshold value");
 		LOG_ERR("Device: %s, error: %d",
-			accel_sensor.dev->name, err);
+			accel_sensor_lp.dev->name, err);
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
 		return err;
@@ -378,10 +439,10 @@ int ext_sensors_accelerometer_trigger_callback_set(bool enable)
 
 	sensor_trigger_handler_t handler = enable ? accelerometer_trigger_handler : NULL;
 
-	err = sensor_trigger_set(accel_sensor.dev, &trig, handler);
+	err = sensor_trigger_set(accel_sensor_lp.dev, &trig, handler);
 	if (err) {
 		LOG_ERR("Could not set trigger for device %s, error: %d",
-			accel_sensor.dev->name, err);
+			accel_sensor_lp.dev->name, err);
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
 		return err;

applications/asset_tracker_v2/src/main.c
@@ -525,7 +525,8 @@ void on_sub_state_passive(struct app_msg_data *msg)
 	}
 
 	if ((IS_EVENT(msg, ui, UI_EVT_BUTTON_DATA_READY)) ||
-	    (IS_EVENT(msg, sensor, SENSOR_EVT_MOVEMENT_DATA_READY))) {
+	    (IS_EVENT(msg, sensor, SENSOR_EVT_MOVEMENT_DATA_READY)) ||
+	    (IS_EVENT(msg, sensor, SENSOR_EVT_IMPACT_DATA_READY))) {
 
 		if (IS_EVENT(msg, ui, UI_EVT_BUTTON_DATA_READY) &&
 		    msg->module.ui.data.ui.button_number != 2) {

applications/asset_tracker_v2/src/modules/data_module.c
@@ -73,6 +73,7 @@ static struct cloud_data_gnss gnss_buf[CONFIG_DATA_GNSS_BUFFER_COUNT];
 static struct cloud_data_sensors sensors_buf[CONFIG_DATA_SENSOR_BUFFER_COUNT];
 static struct cloud_data_ui ui_buf[CONFIG_DATA_UI_BUFFER_COUNT];
 static struct cloud_data_accelerometer accel_buf[CONFIG_DATA_ACCELEROMETER_BUFFER_COUNT];
+static struct cloud_data_impact impact_buf[CONFIG_DATA_IMPACT_BUFFER_COUNT];
 static struct cloud_data_battery bat_buf[CONFIG_DATA_BATTERY_BUFFER_COUNT];
 static struct cloud_data_modem_dynamic modem_dyn_buf[CONFIG_DATA_MODEM_DYNAMIC_BUFFER_COUNT];
 static struct cloud_data_neighbor_cells neighbor_cells;
@@ -92,6 +93,7 @@ static int head_sensor_buf;
 static int head_modem_dyn_buf;
 static int head_ui_buf;
 static int head_accel_buf;
+static int head_impact_buf;
 static int head_bat_buf;
 
 static K_SEM_DEFINE(config_load_sem, 0, 1);
@@ -590,6 +592,7 @@ static void data_encode(void)
 					      &modem_dyn_buf[head_modem_dyn_buf],
 					      &ui_buf[head_ui_buf],
 					      &accel_buf[head_accel_buf],
+					      &impact_buf[head_impact_buf],
 					      &bat_buf[head_bat_buf]);
 		switch (err) {
 		case 0:
@@ -619,6 +622,7 @@ static void data_encode(void)
 						    &modem_stat,
 						    modem_dyn_buf,
 						    ui_buf,
+						    impact_buf,
 						    accel_buf,
 						    bat_buf,
 						    ARRAY_SIZE(gnss_buf),
@@ -626,6 +630,7 @@ static void data_encode(void)
 						    MODEM_STATIC_ARRAY_SIZE,
 						    ARRAY_SIZE(modem_dyn_buf),
 						    ARRAY_SIZE(ui_buf),
+						    ARRAY_SIZE(impact_buf),
 						    ARRAY_SIZE(accel_buf),
 						    ARRAY_SIZE(bat_buf));
 		switch (err) {
@@ -796,6 +801,31 @@ static void data_ui_send(void)
 	}
 
 	data_send(DATA_EVT_UI_DATA_SEND, &codec);
+}
+
+static void data_impact_send(void)
+{
+	int err;
+	struct cloud_codec_data codec = {0};
+
+	if (!date_time_is_valid()) {
+		return;
+	}
+
+	err = cloud_codec_encode_impact_data(&codec, &impact_buf[head_impact_buf]);
+	if (err == -ENODATA) {
+		LOG_DBG("No new impact data to encode, error: %d", err);
+		return;
+	} else if (err == -ENOTSUP) {
+		LOG_WRN("Encoding of impact data is not supported, error: %d", err);
+		return;
+	} else if (err) {
+		LOG_ERR("Encoding impact data failed, error: %d", err);
+		SEND_ERROR(data, DATA_EVT_ERROR, err);
+		return;
+	}
+
+	data_send(DATA_EVT_IMPACT_DATA_SEND, &codec);
 }
 
 static void requested_data_clear(void)
@@ -1098,6 +1128,11 @@ static void on_cloud_state_connected(struct data_msg_data *msg)
 		return;
 	}
 
+	if (IS_EVENT(msg, data, DATA_EVT_IMPACT_DATA_READY)) {
+		data_impact_send();
+		return;
+	}
+
 	if (IS_EVENT(msg, cloud, CLOUD_EVT_DISCONNECTED)) {
 		state_set(STATE_CLOUD_DISCONNECTED);
 		return;
@@ -1322,6 +1357,20 @@ static void on_all_states(struct data_msg_data *msg)
 		cloud_codec_populate_accel_buffer(accel_buf, &new_movement_data,
 						  &head_accel_buf,
 						  ARRAY_SIZE(accel_buf));
+	}
+
+	if (IS_EVENT(msg, sensor, SENSOR_EVT_IMPACT_DATA_READY)) {
+		struct cloud_data_impact new_impact_data = {
+			.magnitude = msg->module.sensor.data.impact.magnitude,
+			.ts = msg->module.sensor.data.impact.timestamp,
+			.queued = true
+		};
+
+		cloud_codec_populate_impact_buffer(impact_buf, &new_impact_data,
+						   &head_impact_buf,
+						   ARRAY_SIZE(impact_buf));
+		SEND_EVENT(data, DATA_EVT_IMPACT_DATA_READY);
+		return;
 	}
 
 	if (IS_EVENT(msg, gnss, GNSS_EVT_DATA_READY)) {

Sandy6Zhou/nrf

applications/asset_tracker_v2/src/ext_sensors/ext_sensors.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <zephyr/drivers/sensor.h>
 #include <stdlib.h>
+#include <math.h>
 
 #if defined(CONFIG_EXTERNAL_SENSORS_BME680_BSEC)
 #include "ext_sensors_bsec.h"
@@ -57,10 +58,19 @@ static struct env_sensor press_sensor = {
 	.dev = DEVICE_DT_GET(DT_ALIAS(pressure_sensor)),
 };
 
-static struct env_sensor accel_sensor = {
+/** Sensor struct for the low-power accelerometer */
+static struct env_sensor accel_sensor_lp = {
 	.channel = SENSOR_CHAN_ACCEL_XYZ,
 	.dev = DEVICE_DT_GET(DT_ALIAS(accelerometer)),
 };
+
+#if defined(CONFIG_EXTERNAL_SENSORS_IMPACT_DETECTION)
+/** Sensor struct for the high-G accelerometer */
+static struct env_sensor accel_sensor_hg = {
+	.channel = SENSOR_CHAN_ACCEL_XYZ,
+	.dev = DEVICE_DT_GET(DT_ALIAS(impact_sensor)),
+};
+#endif
 
 static ext_sensor_handler_t evt_handler;
 
@@ -105,6 +115,44 @@ static void accelerometer_trigger_handler(const struct device *dev,
 	}
 }
 
+#if defined(CONFIG_EXTERNAL_SENSORS_IMPACT_DETECTION)
+static void impact_trigger_handler(const struct device *dev,
+				   const struct sensor_trigger *trig)
+{
+	struct sensor_value data[ACCELEROMETER_CHANNELS];
+	struct ext_sensor_evt evt = {0};
+	int err;
+
+	switch (trig->type) {
+	case SENSOR_TRIG_THRESHOLD:
+		if (sensor_sample_fetch(dev) < 0) {
+			LOG_ERR("Sample fetch error");
+			return;
+		}
+
+		err = sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, data);
+		if (err) {
+			LOG_ERR("sensor_channel_get, error: %d", err);
+			return;
+		}
+
+		evt.value = sqrt(pow(sensor_ms2_to_g(&data[0]), 2.0) +
+				 pow(sensor_ms2_to_g(&data[1]), 2.0) +
+				 pow(sensor_ms2_to_g(&data[2]), 2.0));
+
+		LOG_DBG("Detected impact of %6.2f g\n", evt.value);
+
+		if (evt.value > 0.0) {
+			evt.type = EXT_SENSOR_EVT_ACCELEROMETER_IMPACT_TRIGGER;
+			evt_handler(&evt);
+		}
+		break;
+	default:
+		LOG_ERR("Unknown trigger");
+	}
+}
+#endif
+
 int ext_sensors_init(ext_sensor_handler_t handler)
 {
 	struct ext_sensor_evt evt = {0};
@@ -121,7 +169,7 @@ int ext_sensors_init(ext_sensor_handler_t handler)
 
 	if (err) {
 		LOG_ERR("ext_sensors_bsec_init, error: %d", err);
-		evt.type = EXT_SENSOR_EVT_BME680_ERROR;
+		evt.type = EXT_SENSOR_EVT_BME680_BSEC_ERROR;
 		evt_handler(&evt);
 	}
 #else
@@ -144,12 +192,32 @@ int ext_sensors_init(ext_sensor_handler_t handler)
 	}
 #endif /* if defined(CONFIG_EXTERNAL_SENSORS_BME680_BSEC) */
 
-	if (!device_is_ready(accel_sensor.dev)) {
-		LOG_ERR("Accelerometer device is not ready");
+	if (!device_is_ready(accel_sensor_lp.dev)) {
+		LOG_ERR("Low-power accelerometer device is not ready");
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
 	}
 
+#if defined(CONFIG_EXTERNAL_SENSORS_IMPACT_DETECTION)
+	if (!device_is_ready(accel_sensor_hg.dev)) {
+		LOG_ERR("High-G accelerometer device is not ready");
+		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
+		evt_handler(&evt);
+	} else {
+		struct sensor_trigger trig = {
+			.chan = SENSOR_CHAN_ACCEL_XYZ,
+			.type = SENSOR_TRIG_THRESHOLD
+		};
+
+		int err = sensor_trigger_set(accel_sensor_hg.dev,
+					     &trig, impact_trigger_handler);
+		if (err) {
+			LOG_ERR("Could not set trigger for device %s, error: %d",
+				accel_sensor_hg.dev->name, err);
+			return err;
+		}
+	}
+#endif
 	return 0;
 }
 
@@ -300,34 +368,34 @@ int ext_sensors_mov_thres_set(double threshold_new)
 		.val1 = input_value
 	};
 
-	err = sensor_attr_set(accel_sensor.dev, SENSOR_CHAN_ACCEL_X,
+	err = sensor_attr_set(accel_sensor_lp.dev, SENSOR_CHAN_ACCEL_X,
 			      SENSOR_ATTR_UPPER_THRESH, &data);
 	if (err) {
 		LOG_ERR("Failed to set accelerometer x-axis threshold value");
 		LOG_ERR("Device: %s, error: %d",
-			accel_sensor.dev->name, err);
+			accel_sensor_lp.dev->name, err);
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
 		return err;
 	}
 
-	err = sensor_attr_set(accel_sensor.dev, SENSOR_CHAN_ACCEL_Y,
+	err = sensor_attr_set(accel_sensor_lp.dev, SENSOR_CHAN_ACCEL_Y,
 			      SENSOR_ATTR_UPPER_THRESH, &data);
 	if (err) {
 		LOG_ERR("Failed to set accelerometer y-axis threshold value");
 		LOG_ERR("Device: %s, error: %d",
-			accel_sensor.dev->name, err);
+			accel_sensor_lp.dev->name, err);
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
 		return err;
 	}
 
-	err = sensor_attr_set(accel_sensor.dev, SENSOR_CHAN_ACCEL_Z,
+	err = sensor_attr_set(accel_sensor_lp.dev, SENSOR_CHAN_ACCEL_Z,
 			      SENSOR_ATTR_UPPER_THRESH, &data);
 	if (err) {
 		LOG_ERR("Failed to set accelerometer z-axis threshold value");
 		LOG_ERR("Device: %s, error: %d",
-			accel_sensor.dev->name, err);
+			accel_sensor_lp.dev->name, err);
 		evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 		evt_handler(&evt);
 		return err;
@@ -349,19 +417,19 @@ int ext_sensors_accelerometer_trigger_callback_set(bool enable)
 
 	sensor_trigger_handler_t handler = enable ? accelerometer_trigger_handler : NULL;
 
-	err = sensor_trigger_set(accel_sensor.dev, &trig, handler);
+	err = sensor_trigger_set(accel_sensor_lp.dev, &trig, handler);
 	if (err) {
 		goto error;
 	}
 	trig.type = SENSOR_TRIG_STATIONARY;
-	err = sensor_trigger_set(accel_sensor.dev, &trig, handler);
+	err = sensor_trigger_set(accel_sensor_lp.dev, &trig, handler);
 	if (err) {
 		goto error;
 	}
 		return 0;
 error:
 	LOG_ERR("Could not set trigger for device %s, error: %d",
-		accel_sensor.dev->name, err);
+		accel_sensor_lp.dev->name, err);
 	evt.type = EXT_SENSOR_EVT_ACCELEROMETER_ERROR;
 	evt_handler(&evt);
 	return err;

applications/asset_tracker_v2/src/main.c
@@ -512,8 +512,8 @@ void on_sub_state_passive(struct app_msg_data *msg)
 	}
 
 	if ((IS_EVENT(msg, ui, UI_EVT_BUTTON_DATA_READY)) ||
-	    (IS_EVENT(msg, sensor, SENSOR_EVT_MOVEMENT_ACTIVITY_DETECTED))) {
-
+	    (IS_EVENT(msg, sensor, SENSOR_EVT_MOVEMENT_ACTIVITY_DETECTED)) ||
+	    (IS_EVENT(msg, sensor, SENSOR_EVT_MOVEMENT_IMPACT_DETECTED))) {
 		if (IS_EVENT(msg, ui, UI_EVT_BUTTON_DATA_READY) &&
 		    msg->module.ui.data.ui.button_number != 2) {
 			return;

applications/asset_tracker_v2/src/modules/data_module.c
@@ -73,6 +73,7 @@ static struct cloud_data_gnss gnss_buf[CONFIG_DATA_GNSS_BUFFER_COUNT];
 static struct cloud_data_sensors sensors_buf[CONFIG_DATA_SENSOR_BUFFER_COUNT];
 static struct cloud_data_ui ui_buf[CONFIG_DATA_UI_BUFFER_COUNT];
 static struct cloud_data_accelerometer accel_buf[CONFIG_DATA_ACCELEROMETER_BUFFER_COUNT];
+static struct cloud_data_impact impact_buf[CONFIG_DATA_IMPACT_BUFFER_COUNT];
 static struct cloud_data_battery bat_buf[CONFIG_DATA_BATTERY_BUFFER_COUNT];
 static struct cloud_data_modem_dynamic modem_dyn_buf[CONFIG_DATA_MODEM_DYNAMIC_BUFFER_COUNT];
 static struct cloud_data_neighbor_cells neighbor_cells;
@@ -92,6 +93,7 @@ static int head_sensor_buf;
 static int head_modem_dyn_buf;
 static int head_ui_buf;
 static int head_accel_buf;
+static int head_impact_buf;
 static int head_bat_buf;
 
 static K_SEM_DEFINE(config_load_sem, 0, 1);
@@ -591,6 +593,7 @@ static void data_encode(void)
 					      &modem_dyn_buf[head_modem_dyn_buf],
 					      &ui_buf[head_ui_buf],
 					      &accel_buf[head_accel_buf],
+					      &impact_buf[head_impact_buf],
 					      &bat_buf[head_bat_buf]);
 		switch (err) {
 		case 0:
@@ -620,6 +623,7 @@ static void data_encode(void)
 						    &modem_stat,
 						    modem_dyn_buf,
 						    ui_buf,
+						    impact_buf,
 						    accel_buf,
 						    bat_buf,
 						    ARRAY_SIZE(gnss_buf),
@@ -627,6 +631,7 @@ static void data_encode(void)
 						    MODEM_STATIC_ARRAY_SIZE,
 						    ARRAY_SIZE(modem_dyn_buf),
 						    ARRAY_SIZE(ui_buf),
+						    ARRAY_SIZE(impact_buf),
 						    ARRAY_SIZE(accel_buf),
 						    ARRAY_SIZE(bat_buf));
 		switch (err) {
@@ -797,6 +802,31 @@ static void data_ui_send(void)
 	}
 
 	data_send(DATA_EVT_UI_DATA_SEND, &codec);
+}
+
+static void data_impact_send(void)
+{
+	int err;
+	struct cloud_codec_data codec = {0};
+
+	if (!date_time_is_valid()) {
+		return;
+	}
+
+	err = cloud_codec_encode_impact_data(&codec, &impact_buf[head_impact_buf]);
+	if (err == -ENODATA) {
+		LOG_DBG("No new impact data to encode, error: %d", err);
+		return;
+	} else if (err == -ENOTSUP) {
+		LOG_WRN("Encoding of impact data is not supported, error: %d", err);
+		return;
+	} else if (err) {
+		LOG_ERR("Encoding impact data failed, error: %d", err);
+		SEND_ERROR(data, DATA_EVT_ERROR, err);
+		return;
+	}
+
+	data_send(DATA_EVT_IMPACT_DATA_SEND, &codec);
 }
 
 static void requested_data_clear(void)
@@ -1094,6 +1124,11 @@ static void on_cloud_state_connected(struct data_msg_data *msg)
 		return;
 	}
 
+	if (IS_EVENT(msg, data, DATA_EVT_IMPACT_DATA_READY)) {
+		data_impact_send();
+		return;
+	}
+
 	if (IS_EVENT(msg, cloud, CLOUD_EVT_DISCONNECTED)) {
 		state_set(STATE_CLOUD_DISCONNECTED);
 		return;
@@ -1318,6 +1353,20 @@ static void on_all_states(struct data_msg_data *msg)
 		cloud_codec_populate_accel_buffer(accel_buf, &new_movement_data,
 						  &head_accel_buf,
 						  ARRAY_SIZE(accel_buf));
+	}
+
+	if (IS_EVENT(msg, sensor, SENSOR_EVT_MOVEMENT_IMPACT_DETECTED)) {
+		struct cloud_data_impact new_impact_data = {
+			.magnitude = msg->module.sensor.data.impact.magnitude,
+			.ts = msg->module.sensor.data.impact.timestamp,
+			.queued = true
+		};
+
+		cloud_codec_populate_impact_buffer(impact_buf, &new_impact_data,
+						   &head_impact_buf,
+						   ARRAY_SIZE(impact_buf));
+		SEND_EVENT(data, DATA_EVT_IMPACT_DATA_READY);
+		return;
 	}
 
 	if (IS_EVENT(msg, gnss, GNSS_EVT_DATA_READY)) {