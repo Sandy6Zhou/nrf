stig-bjorlykke/sdk-nrf

lib: pdn: Handle CGEV ME DETACH and NW DETACH
Handle +CGEV: ME DETACH and +CGEV: NW DETACH notifications in on_cgev().
This notification does not contain <cid> and will be sent to all PDN
contexts. It will be sent instead of ME PDN DEACT and NW PDN DEACT on
power off in an upcoming nRF9160 modem release.

Signed-off-by: Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>

lib/pdn/pdn.c
@@ -120,7 +120,7 @@ static void on_cnec_esm(const char *notif)
 static void on_cgev(const char *notif)
 {
 	char *p;
-	uint8_t cid;
+	int8_t cid;
 	struct pdn *pdn;
 
 	const struct {
@@ -130,6 +130,8 @@ static void on_cgev(const char *notif)
 		{"ME PDN ACT",	 PDN_EVENT_ACTIVATED},	 /* +CGEV: ME PDN ACT <cid>[,<reason>] */
 		{"ME PDN DEACT", PDN_EVENT_DEACTIVATED}, /* +CGEV: ME PDN DEACT <cid> */
 		{"NW PDN DEACT", PDN_EVENT_DEACTIVATED}, /* +CGEV: NW PDN DEACT <cid> */
+		{"ME DETACH",	 PDN_EVENT_DETACHED},	 /* +CGEV: ME DETACH */
+		{"NW DETACH",	 PDN_EVENT_DETACHED},	 /* +CGEV: NW DETACH */
 		/* Order is important */
 		{"IPV6 FAIL",	 PDN_EVENT_IPV6_DOWN},	 /* +CGEV: IPV6 FAIL <cid> */
 		{"IPV6",	 PDN_EVENT_IPV6_UP},	 /* +CGEV: IPV6 <cid> */
@@ -141,7 +143,13 @@ static void on_cgev(const char *notif)
 			continue;
 		}
 
-		cid = strtoul(p + strlen(map[i].notif), &p, 10);
+		p += strlen(map[i].notif);
+		if (*p == ' ') {
+			cid = strtoul(p, &p, 10);
+		} else {
+			cid = CID_UNASSIGNED;
+		}
+
 		if (cid == pdn_act_notif.cid && map[i].event == PDN_EVENT_ACTIVATED) {
 			if (*p == ',') {
 				pdn_act_notif.reason = strtol(p + 1, NULL, 10);
@@ -152,8 +160,8 @@ static void on_cgev(const char *notif)
 		}
 
 		SYS_SLIST_FOR_EACH_CONTAINER(&pdn_contexts, pdn, node) {
-			if (pdn->context_id == cid && pdn->callback) {
-				pdn->callback(cid, map[i].event, 0);
+			if ((pdn->context_id == cid || cid == CID_UNASSIGNED) && pdn->callback) {
+				pdn->callback(pdn->context_id, map[i].event, 0);
 			}
 		}
 

Sandy6Zhou/nrf

lib/pdn/pdn.c
@@ -120,7 +120,7 @@ static void on_cnec_esm(const char *notif)
 static void on_cgev(const char *notif)
 {
 	char *p;
-	uint8_t cid;
+	int8_t cid;
 	struct pdn *pdn;
 
 	const struct {
@@ -130,6 +130,8 @@ static void on_cgev(const char *notif)
 		{"ME PDN ACT",	 PDN_EVENT_ACTIVATED},	 /* +CGEV: ME PDN ACT <cid>[,<reason>] */
 		{"ME PDN DEACT", PDN_EVENT_DEACTIVATED}, /* +CGEV: ME PDN DEACT <cid> */
 		{"NW PDN DEACT", PDN_EVENT_DEACTIVATED}, /* +CGEV: NW PDN DEACT <cid> */
+		{"ME DETACH",	 PDN_EVENT_NETWORK_DETACH},	 /* +CGEV: ME DETACH */
+		{"NW DETACH",	 PDN_EVENT_NETWORK_DETACH},	 /* +CGEV: NW DETACH */
 		/* Order is important */
 		{"IPV6 FAIL",	 PDN_EVENT_IPV6_DOWN},	 /* +CGEV: IPV6 FAIL <cid> */
 		{"IPV6",	 PDN_EVENT_IPV6_UP},	 /* +CGEV: IPV6 <cid> */
@@ -141,7 +143,13 @@ static void on_cgev(const char *notif)
 			continue;
 		}
 
-		cid = strtoul(p + strlen(map[i].notif), &p, 10);
+		p += strlen(map[i].notif);
+		if (*p == ' ') {
+			cid = strtoul(p, &p, 10);
+		} else {
+			cid = CID_UNASSIGNED;
+		}
+
 		if (cid == pdn_act_notif.cid && map[i].event == PDN_EVENT_ACTIVATED) {
 			if (*p == ',') {
 				pdn_act_notif.reason = strtol(p + 1, NULL, 10);
@@ -152,8 +160,8 @@ static void on_cgev(const char *notif)
 		}
 
 		SYS_SLIST_FOR_EACH_CONTAINER(&pdn_contexts, pdn, node) {
-			if (pdn->context_id == cid && pdn->callback) {
-				pdn->callback(cid, map[i].event, 0);
+			if ((pdn->context_id == cid || cid == CID_UNASSIGNED) && pdn->callback) {
+				pdn->callback(pdn->context_id, map[i].event, 0);
 			}
 		}
 