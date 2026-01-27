nrfconnect/sdk-nrf

net/lib/nrf_cloud: Remove log_strdup invocations
It is no longer needed to call log_strdup while providing string
for LOG_ macro.

Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

subsys/net/lib/nrf_cloud/src/nrf_cloud_pgps_utils.c
@@ -65,7 +65,7 @@ static int settings_set(const char *key, size_t len_rd,
 		return -EINVAL;
 	}
 
-	LOG_DBG("Settings key:%s, size:%d", log_strdup(key), len_rd);
+	LOG_DBG("Settings key:%s, size:%d", key, len_rd);
 
 	if (!strncmp(key, SETTINGS_KEY_PGPS_HEADER,
 		     strlen(SETTINGS_KEY_PGPS_HEADER)) &&
@@ -352,7 +352,7 @@ void npgps_print_blocks(void)
 		map[i] = pool.block_used[i] ? '1' : '0';
 	}
 	map[i] = '\0';
-	LOG_INF("map:%s", log_strdup(map));
+	LOG_INF("map:%s", map);
 }
 
 

Sandy6Zhou/nrf

subsys/net/lib/nrf_cloud/src/nrf_cloud_pgps_utils.c
@@ -65,7 +65,7 @@ static int settings_set(const char *key, size_t len_rd,
 		return -EINVAL;
 	}
 
-	LOG_DBG("Settings key:%s, size:%d", log_strdup(key), len_rd);
+	LOG_DBG("Settings key:%s, size:%d", key, len_rd);
 
 	if (!strncmp(key, SETTINGS_KEY_PGPS_HEADER,
 		     strlen(SETTINGS_KEY_PGPS_HEADER)) &&
@@ -372,7 +372,7 @@ void npgps_print_blocks(void)
 		map[i] = pool.block_used[i] ? '1' : '0';
 	}
 	map[i] = '\0';
-	LOG_DBG("map:%s", log_strdup(map));
+	LOG_DBG("map:%s", map);
 }
 
 