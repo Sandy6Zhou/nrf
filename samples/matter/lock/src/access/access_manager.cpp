nrfconnect/sdk-nrf

samples: matter: lock: Add ValidatePINResult
Return `ValidatePINResult` containing user id and credential used for
validation.

Signed-off-by: Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>

samples/matter/lock/src/access/access_manager.cpp
@@ -54,7 +54,8 @@ bool AccessManager<CRED_BIT_MASK>::ValidateCustom(CredentialTypeEnum type, chip:
 }
 
 template <CredentialsBits CRED_BIT_MASK>
-bool AccessManager<CRED_BIT_MASK>::ValidatePIN(const Optional<ByteSpan> &pinCode, OperationErrorEnum &err)
+bool AccessManager<CRED_BIT_MASK>::ValidatePIN(const Optional<ByteSpan> &pinCode, OperationErrorEnum &err,
+					       Nullable<ValidatePINResult> &result)
 {
 	/* Optionality of the PIN code is validated by the caller, so assume it is OK not to provide the PIN
 	 * code. */
@@ -72,6 +73,18 @@ bool AccessManager<CRED_BIT_MASK>::ValidatePIN(const Optional<ByteSpan> &pinCode
 			}
 
 			if (credential.credentialData.data_equal(pinCode.Value())) {
+				uint32_t credentialUserId;
+				if (GetCredentialUserId(index, CredentialTypeEnum::kPin, credentialUserId) ==
+				    CHIP_NO_ERROR) {
+					result = ValidatePINResult{
+						.userId = static_cast<uint16_t>(credentialUserId),
+						.credential = LockOpCredentials{ CredentialTypeEnum::kPin,
+										 static_cast<uint16_t>(index) },
+					};
+				} else {
+					result = {};
+				}
+
 				LOG_DBG("Valid lock PIN code provided");
 				return true;
 			}

samples/matter/lock/src/access/access_manager.h
@@ -13,6 +13,11 @@
 
 template <DoorLockData::CredentialsBits CRED_BIT_MASK> class AccessManager {
 public:
+	struct ValidatePINResult {
+		uint16_t userId;
+		LockOpCredentials credential;
+	};
+
 	/**
 	 * @brief Signature of the callback fired when the credential is set.
 	 *
@@ -234,9 +239,11 @@ public:
 	 *
 	 * @param pinCode PIN code data.
 	 * @param err specific error code enumeration.
+	 * @param result user and credential data set on success.
 	 * @return true on success, false otherwise.
 	 */
-	bool ValidatePIN(const Optional<chip::ByteSpan> &pinCode, OperationErrorEnum &err);
+	bool ValidatePIN(const Optional<chip::ByteSpan> &pinCode, OperationErrorEnum &err,
+			 Nullable<ValidatePINResult> &result);
 	bool ValidateCustom(CredentialTypeEnum type, chip::MutableByteSpan &secret);
 
 	/**

Sandy6Zhou/nrf

samples/matter/lock/src/access/access_manager.cpp
@@ -54,7 +54,8 @@ bool AccessManager<CRED_BIT_MASK>::ValidateCustom(CredentialTypeEnum type, chip:
 }
 
 template <CredentialsBits CRED_BIT_MASK>
-bool AccessManager<CRED_BIT_MASK>::ValidatePIN(const Optional<ByteSpan> &pinCode, OperationErrorEnum &err)
+bool AccessManager<CRED_BIT_MASK>::ValidatePIN(const Optional<ByteSpan> &pinCode, OperationErrorEnum &err,
+					       Nullable<ValidatePINResult> &result)
 {
 	/* Optionality of the PIN code is validated by the caller, so assume it is OK not to provide the PIN
 	 * code. */
@@ -72,6 +73,18 @@ bool AccessManager<CRED_BIT_MASK>::ValidatePIN(const Optional<ByteSpan> &pinCode
 			}
 
 			if (credential.credentialData.data_equal(pinCode.Value())) {
+				uint32_t credentialUserId;
+				if (GetCredentialUserId(index, CredentialTypeEnum::kPin, credentialUserId) ==
+				    CHIP_NO_ERROR) {
+					result = ValidatePINResult{
+						.mUserId = static_cast<uint16_t>(credentialUserId),
+						.mCredential = LockOpCredentials{ CredentialTypeEnum::kPin,
+										  static_cast<uint16_t>(index) },
+					};
+				} else {
+					result = {};
+				}
+
 				LOG_DBG("Valid lock PIN code provided");
 				return true;
 			}

samples/matter/lock/src/access/access_manager.h
@@ -13,6 +13,11 @@
 
 template <DoorLockData::CredentialsBits CRED_BIT_MASK> class AccessManager {
 public:
+	struct ValidatePINResult {
+		uint16_t mUserId;
+		LockOpCredentials mCredential;
+	};
+
 	/**
 	 * @brief Signature of the callback fired when the credential is set.
 	 *
@@ -234,9 +239,11 @@ public:
 	 *
 	 * @param pinCode PIN code data.
 	 * @param err specific error code enumeration.
+	 * @param result user and credential data set on success.
 	 * @return true on success, false otherwise.
 	 */
-	bool ValidatePIN(const Optional<chip::ByteSpan> &pinCode, OperationErrorEnum &err);
+	bool ValidatePIN(const Optional<chip::ByteSpan> &pinCode, OperationErrorEnum &err,
+			 Nullable<ValidatePINResult> &result);
 	bool ValidateCustom(CredentialTypeEnum type, chip::MutableByteSpan &secret);
 
 	/**