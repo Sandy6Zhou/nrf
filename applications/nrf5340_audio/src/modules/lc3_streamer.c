koffes/sdk-nrf

applications: nrf5340_audio: Add helper functions to lc3_streamer
Add helper functions to return the file path or the loop setting
for a lc3_stream.

OCT-3109

Signed-off-by: Andreas Vibeto <andreas.vibeto@nordicsemi.no>

applications/nrf5340_audio/src/modules/lc3_streamer.c
@@ -350,6 +350,38 @@ uint8_t lc3_streamer_num_active_streams(void)
 	return num_active;
 }
 
+int lc3_streamer_file_path_get(const uint8_t streamer_idx, char *const path, const size_t path_len)
+{
+	if (streamer_idx >= ARRAY_SIZE(streams)) {
+		LOG_ERR("Invalid streamer index %d", streamer_idx);
+		return -EINVAL;
+	}
+
+	if (path == NULL) {
+		LOG_ERR("Nullptr received for path");
+		return -EINVAL;
+	}
+
+	if (path_len < strlen(streams[streamer_idx].filename)) {
+		LOG_ERR("Path buffer too small");
+		return -EINVAL;
+	}
+
+	strncpy(path, streams[streamer_idx].filename, path_len);
+
+	return 0;
+}
+
+bool lc3_streamer_is_looping(const uint8_t streamer_idx)
+{
+	if (streamer_idx >= ARRAY_SIZE(streams)) {
+		LOG_ERR("Invalid streamer index %d", streamer_idx);
+		return false;
+	}
+
+	return streams[streamer_idx].loop_stream;
+}
+
 int lc3_streamer_stream_close(const uint8_t streamer_idx)
 {
 	int ret;

Sandy6Zhou/nrf

applications/nrf5340_audio/src/modules/lc3_streamer.c
@@ -100,6 +100,7 @@ static int stream_close(struct lc3_stream *stream)
 	}
 
 	stream->state = STREAM_IDLE;
+	memset(stream->filename, 0, sizeof(stream->filename));
 
 	return 0;
 }
@@ -348,6 +349,37 @@ uint8_t lc3_streamer_num_active_streams(void)
 	}
 
 	return num_active;
+}
+
+int lc3_streamer_file_path_get(const uint8_t streamer_idx, char *const path, const size_t path_len)
+{
+	if (streamer_idx >= ARRAY_SIZE(streams)) {
+		LOG_ERR("Invalid streamer index %d", streamer_idx);
+		return -EINVAL;
+	}
+
+	if (path == NULL) {
+		LOG_ERR("Nullptr received for path");
+		return -EINVAL;
+	}
+
+	if (path_len < strlen(streams[streamer_idx].filename)) {
+		LOG_WRN("Path buffer too small");
+	}
+
+	strncpy(path, streams[streamer_idx].filename, path_len);
+
+	return 0;
+}
+
+bool lc3_streamer_is_looping(const uint8_t streamer_idx)
+{
+	if (streamer_idx >= ARRAY_SIZE(streams)) {
+		LOG_ERR("Invalid streamer index %d", streamer_idx);
+		return false;
+	}
+
+	return streams[streamer_idx].loop_stream;
 }
 
 int lc3_streamer_stream_close(const uint8_t streamer_idx)