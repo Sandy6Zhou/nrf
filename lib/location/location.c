maxd-nordic/sdk-nrf

lib: location: External Wi-Fi support
Support for handling location request for Wi-Fi access point list
within the application towards cloud.

Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

lib/location/location.c
@@ -237,3 +237,12 @@ void location_cellular_ext_result_set(
 	location_core_cellular_ext_result_set(result, location);
 #endif
 }
+
+void location_wifi_ext_result_set(
+	enum location_wifi_ext_result result,
+	struct location_data *location)
+{
+#if defined(CONFIG_LOCATION_METHOD_WIFI_EXTERNAL)
+	location_core_wifi_ext_result_set(result, location);
+#endif
+}

lib/location/location_core.h
@@ -41,6 +41,13 @@ void location_core_cellular_ext_result_set(
 	struct location_data *location);
 #endif
 
+#if defined(CONFIG_LOCATION_METHOD_WIFI_EXTERNAL)
+void location_core_event_cb_wifi_request(struct wifi_scan_info *request);
+void location_core_wifi_ext_result_set(
+	enum location_wifi_ext_result result,
+	struct location_data *location);
+#endif
+
 void location_core_config_log(const struct location_config *config);
 void location_core_timer_start(int32_t timeout);
 void location_core_timer_stop(void);

Sandy6Zhou/nrf

lib/location/location.c
@@ -230,10 +230,19 @@ int location_pgps_data_process(const char *buf, size_t buf_len)
 }
 
 void location_cellular_ext_result_set(
-	enum location_cellular_ext_result result,
+	enum location_ext_result result,
 	struct location_data *location)
 {
 #if defined(CONFIG_LOCATION_METHOD_CELLULAR_EXTERNAL)
 	location_core_cellular_ext_result_set(result, location);
+#endif
+}
+
+void location_wifi_ext_result_set(
+	enum location_ext_result result,
+	struct location_data *location)
+{
+#if defined(CONFIG_LOCATION_METHOD_WIFI_EXTERNAL)
+	location_core_wifi_ext_result_set(result, location);
 #endif
 }

lib/location/location_core.h
@@ -38,7 +38,14 @@ void location_core_event_cb_pgps_request(const struct gps_pgps_request *request)
 #if defined(CONFIG_LOCATION_METHOD_CELLULAR_EXTERNAL)
 void location_core_event_cb_cellular_request(struct lte_lc_cells_info *request);
 void location_core_cellular_ext_result_set(
-	enum location_cellular_ext_result result,
+	enum location_ext_result result,
+	struct location_data *location);
+#endif
+
+#if defined(CONFIG_LOCATION_METHOD_WIFI_EXTERNAL)
+void location_core_event_cb_wifi_request(struct wifi_scan_info *request);
+void location_core_wifi_ext_result_set(
+	enum location_ext_result result,
 	struct location_data *location);
 #endif
 