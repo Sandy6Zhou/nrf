gchwier/sdk-nrf

tests: Use correct KMU keyslot during provisioning
Updated tests to use proper keyname for MCUboot

Signed-off-by: Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>

tests/subsys/kmu/pytest/common.py
@@ -9,6 +9,7 @@ import shlex
 import subprocess
 
 from pathlib import Path
+from twister_harness.helpers.utils import find_in_config
 
 logger = logging.getLogger(__name__)
 
@@ -71,6 +72,14 @@ def flash_board(build_dir: Path | str, dev_id: str | None, erase: bool = False):
     if erase:
         command.extend(['--erase'])
     run_command(command)
+
+
+def get_keyname_for_mcuboot(sysbuild_config: Path) -> str:
+    keyname = "BL_PUBKEY"
+    if (find_in_config(sysbuild_config, "SB_CONFIG_SECURE_BOOT_APPCORE")
+            or find_in_config(sysbuild_config, "SB_CONFIG_MCUBOOT_SIGNATURE_KMU_ORIGINAL_MAPPING")):
+        keyname = "UROT_PUBKEY"
+    return keyname
 
 
 def provision_keys_for_kmu(

Sandy6Zhou/nrf

tests/subsys/kmu/pytest/common.py
@@ -9,6 +9,7 @@ import shlex
 import subprocess
 
 from pathlib import Path
+from twister_harness.helpers.utils import find_in_config
 
 logger = logging.getLogger(__name__)
 
@@ -71,6 +72,14 @@ def flash_board(build_dir: Path | str, dev_id: str | None, erase: bool = False):
     if erase:
         command.extend(['--erase'])
     run_command(command)
+
+
+def get_keyname_for_mcuboot(sysbuild_config: Path) -> str:
+    keyname = "BL_PUBKEY"
+    if (find_in_config(sysbuild_config, "SB_CONFIG_SECURE_BOOT_APPCORE")
+            or find_in_config(sysbuild_config, "SB_CONFIG_MCUBOOT_SIGNATURE_KMU_UROT_MAPPING")):
+        keyname = "UROT_PUBKEY"
+    return keyname
 
 
 def provision_keys_for_kmu(