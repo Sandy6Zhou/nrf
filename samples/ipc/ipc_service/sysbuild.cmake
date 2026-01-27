de-nordic/fw-nrfconnect-nrf

cmake: adjust PM related CMake variables to global properties
Upstream Zephyr PR#57884 adjusted the inclusion of sysbuild modules
so that these are now sourced in a different scope.

This cause PM related variables to not be available when running
partition manager CMake code as part of sysbuild.

PM related variables are therefore changed to global properties, making
the partition manager related code more robust.

Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

samples/ipc/ipc_service/sysbuild.cmake
@@ -10,12 +10,12 @@ ExternalZephyrProject_Add(
   SOURCE_DIR ${APP_DIR}/remote
   BOARD ${SB_CONFIG_REMOTE_BOARD}
 )
-list(APPEND PM_DOMAINS CPUNET)
-set(PM_CPUNET_IMAGES remote)
-set(DOMAIN_APP_CPUNET remote)
+set_property(GLOBAL APPEND PROPERTY PM_DOMAINS CPUNET)
+set_property(GLOBAL APPEND PROPERTY PM_CPUNET_IMAGES remote)
+set_property(GLOBAL PROPERTY DOMAIN_APP_CPUNET remote)
 set(CPUNET_PM_DOMAIN_DYNAMIC_PARTITION remote CACHE INTERNAL "")
 
 # Add a dependency so that the remote sample will be built and flashed first
 add_dependencies(ipc_service remote)
-# Place remote image first in the image list
-set(IMAGES "remote" ${IMAGES})
+# Add dependency so that the remote image is flashed first.
+sysbuild_add_dependencies(FLASH event_manager_proxy remote)

samples/multicore/hello_world/sysbuild.cmake
@@ -10,12 +10,12 @@ ExternalZephyrProject_Add(
     SOURCE_DIR ${APP_DIR}/remote
     BOARD ${SB_CONFIG_REMOTE_BOARD}
   )
-list(APPEND PM_DOMAINS CPUNET)
-set(PM_CPUNET_IMAGES remote)
-set(DOMAIN_APP_CPUNET remote)
+set_property(GLOBAL APPEND PROPERTY PM_DOMAINS CPUNET)
+set_property(GLOBAL APPEND PROPERTY PM_CPUNET_IMAGES remote)
+set_property(GLOBAL PROPERTY DOMAIN_APP_CPUNET remote)
 set(CPUNET_PM_DOMAIN_DYNAMIC_PARTITION remote CACHE INTERNAL "")
 
 # Add a dependency so that the remote sample will be built and flashed first
 add_dependencies(hello_world remote)
-# Place remote image first in the image list
-set(IMAGES "remote" ${IMAGES})
+# Add dependency so that the remote image is flashed first.
+sysbuild_add_dependencies(FLASH event_manager_proxy remote)

Sandy6Zhou/nrf

samples/ipc/ipc_service/sysbuild.cmake
@@ -10,12 +10,12 @@ ExternalZephyrProject_Add(
   SOURCE_DIR ${APP_DIR}/remote
   BOARD ${SB_CONFIG_REMOTE_BOARD}
 )
-list(APPEND PM_DOMAINS CPUNET)
-set(PM_CPUNET_IMAGES remote)
-set(DOMAIN_APP_CPUNET remote)
+set_property(GLOBAL APPEND PROPERTY PM_DOMAINS CPUNET)
+set_property(GLOBAL APPEND PROPERTY PM_CPUNET_IMAGES remote)
+set_property(GLOBAL PROPERTY DOMAIN_APP_CPUNET remote)
 set(CPUNET_PM_DOMAIN_DYNAMIC_PARTITION remote CACHE INTERNAL "")
 
 # Add a dependency so that the remote sample will be built and flashed first
 add_dependencies(ipc_service remote)
-# Place remote image first in the image list
-set(IMAGES "remote" ${IMAGES})
+# Add dependency so that the remote image is flashed first.
+sysbuild_add_dependencies(FLASH ipc_service remote)

samples/multicore/hello_world/sysbuild.cmake
@@ -10,12 +10,12 @@ ExternalZephyrProject_Add(
     SOURCE_DIR ${APP_DIR}/remote
     BOARD ${SB_CONFIG_REMOTE_BOARD}
   )
-list(APPEND PM_DOMAINS CPUNET)
-set(PM_CPUNET_IMAGES remote)
-set(DOMAIN_APP_CPUNET remote)
+set_property(GLOBAL APPEND PROPERTY PM_DOMAINS CPUNET)
+set_property(GLOBAL APPEND PROPERTY PM_CPUNET_IMAGES remote)
+set_property(GLOBAL PROPERTY DOMAIN_APP_CPUNET remote)
 set(CPUNET_PM_DOMAIN_DYNAMIC_PARTITION remote CACHE INTERNAL "")
 
 # Add a dependency so that the remote sample will be built and flashed first
 add_dependencies(hello_world remote)
-# Place remote image first in the image list
-set(IMAGES "remote" ${IMAGES})
+# Add dependency so that the remote image is flashed first.
+sysbuild_add_dependencies(FLASH hello_world remote)