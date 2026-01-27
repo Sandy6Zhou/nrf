krish2718/sdk-nrf

wifi_prov: Rejig the library to use for multiple transports
We now have a new application for this library which will be used with
no transport, so, move the the core provisioning stuff to a new library
that can be used by BLE or no transport.

Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

subsys/bluetooth/services/wifi_prov/wifi_prov_ble.c
@@ -22,9 +22,7 @@
 #include <zephyr/net_buf.h>
 
 #include <zephyr/net/wifi_credentials.h>
-#include <bluetooth/services/wifi_provisioning.h>
-
-#include "wifi_prov_internal.h"
+#include <net/wifi_prov/wifi_prov.h>
 
 LOG_MODULE_DECLARE(wifi_prov, CONFIG_BT_WIFI_PROV_LOG_LEVEL);
 

Sandy6Zhou/nrf

subsys/bluetooth/services/wifi_prov/wifi_prov_ble.c
@@ -22,9 +22,8 @@
 #include <zephyr/net_buf.h>
 
 #include <zephyr/net/wifi_credentials.h>
+#include <net/wifi_prov_core/wifi_prov_core.h>
 #include <bluetooth/services/wifi_provisioning.h>
-
-#include "wifi_prov_internal.h"
 
 LOG_MODULE_DECLARE(wifi_prov, CONFIG_BT_WIFI_PROV_LOG_LEVEL);
 